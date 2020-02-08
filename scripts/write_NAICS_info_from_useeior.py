e7f1caa3bf60db5c1a7b6e451b5fc18dcd73504c
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-08 11:24:25 -0500

    Add NAICS code crosswalk and name tables using script that retrieves them from useeior

---
+++ scripts/write_NAICS_info_from_useeior.py
@@ -0,0 +1,20 @@
+"""
+A script to get NAICS names and a NAICS 2-3-4-5-6 crosswalk from useeior amd store them as .csv
+Depends on rpy2 and tzlocal as well as having R installed and useeior installed
+"""
+
+from flowsa.common import datapath
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
+useeior = importr('useeior')
+
+NAICS_crosswalk = useeior.getNAICS2to6Digits(2012)
+NAICS_crosswalk = pandas2ri.ri2py_dataframe(NAICS_crosswalk)
+NAICS_crosswalk.to_csv(datapath+"NAICS_2012_2to6_Crosswalk.csv",index=False)
+
+NAICS_names = useeior.getNAICSCodeName(2012)
+NAICS_names = pandas2ri.ri2py_dataframe(NAICS_names)
+NAICS_names.to_csv(datapath+"NAICS_2012_Names.csv",index=False)
+
