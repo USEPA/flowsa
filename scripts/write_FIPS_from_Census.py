eef2cd39f470dc910471895aa0ed854506feee0b
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-13 14:22:03 -0500

    -Wrap in a main clause

--- scripts/write_FIPS_from_Census.py
+++ scripts/write_FIPS_from_Census.py
@@ -1,68 +1,72 @@
+"""
+Grabs FIPS 2017 codes from a static URL
+Shapes the set to include State and County names for all records
+Writes reshaped file to datapath as csv
+"""
 import pandas as pd
 from flowsa.common import datapath
 
 #2017 State, County, Minor Civil Division, and Incorporated Place FIPS Codes
-
 url = "https://www2.census.gov/programs-surveys/popest/geographies/2017/all-geocodes-v2017.xlsx"
 
-#Read directly into a pandas df,
-raw_df = pd.read_excel(url)
 
-#skip the first few rows
-FIPS_df = pd.DataFrame(raw_df.loc[4:]).reindex()
-#Assign the column titles
-FIPS_df.columns = raw_df.loc[3,]
+if __name__ == '__main__':
+    #Read directly into a pandas df,
+    raw_df = pd.read_excel(url)
 
-original_cols = FIPS_df.columns
+    #skip the first few rows
+    FIPS_df = pd.DataFrame(raw_df.loc[4:]).reindex()
+    #Assign the column titles
+    FIPS_df.columns = raw_df.loc[3,]
 
-#Create a dictionary of geographic levels
-geocode_levels = {"010":"Country",
-                  "040":"State",
-                  "050":"County"}
-level_codes = geocode_levels.keys()
-#filter df for records with the levels of interest
-FIPS_df = FIPS_df[FIPS_df["Summary Level"].isin(level_codes)]
+    original_cols = FIPS_df.columns
 
-#split df by level to return a list of dfs
-#use a list comprehension to split it out
-FIPS_bylevel = [pd.DataFrame(y) for x,y in FIPS_df.groupby("Summary Level", as_index=False)]
+    #Create a dictionary of geographic levels
+    geocode_levels = {"010":"Country",
+                      "040":"State",
+                      "050":"County"}
+    level_codes = geocode_levels.keys()
+    #filter df for records with the levels of interest
+    FIPS_df = FIPS_df[FIPS_df["Summary Level"].isin(level_codes)]
 
-#Assume df order in list is in geolevels keys order
+    #split df by level to return a list of dfs
+    #use a list comprehension to split it out
+    FIPS_bylevel = [pd.DataFrame(y) for x,y in FIPS_df.groupby("Summary Level", as_index=False)]
 
-state_and_county_fields = {"Country":["State Code (FIPS)"], #country does not have its own field
-                           "State":["State Code (FIPS)"],
-                           "County":["State Code (FIPS)","County Code (FIPS)"]}
+    #Assume df order in list is in geolevels keys order
 
-name_field = "Area Name (including legal/statistical area description)"
+    state_and_county_fields = {"Country":["State Code (FIPS)"], #country does not have its own field
+                               "State":["State Code (FIPS)"],
+                               "County":["State Code (FIPS)","County Code (FIPS)"]}
 
-new_dfs = {}
-for df in FIPS_bylevel:
-    df = df.reset_index(drop=True)
-    level = geocode_levels[df.loc[0,"Summary Level"]]
-    print(level)
-    new_df = df[original_cols]
-    new_df = new_df.rename(columns={name_field:level})
-    fields_to_keep = [str(x) for x in state_and_county_fields[level]]
-    fields_to_keep.append(level)
-    print(fields_to_keep)
-    new_df = new_df[fields_to_keep]
-    #Now left merge each with the main df to take on the columns with level names but not removing existing records
-    new_dfs[level] = new_df
+    name_field = "Area Name (including legal/statistical area description)"
+
+    new_dfs = {}
+    for df in FIPS_bylevel:
+        df = df.reset_index(drop=True)
+        level = geocode_levels[df.loc[0,"Summary Level"]]
+        new_df = df[original_cols]
+        new_df = new_df.rename(columns={name_field:level})
+        fields_to_keep = [str(x) for x in state_and_county_fields[level]]
+        fields_to_keep.append(level)
+        new_df = new_df[fields_to_keep]
+        #Write each to the list
+        new_dfs[level] = new_df
 
 
-#New merge the new dfs to add the info
-#FIPS_df_new = FIPS_df
-for k,v in new_dfs.items():
-    fields_to_merge = [str(x) for x in state_and_county_fields[k]]
-    #FIPS_df_new = pd.merge(FIPS_df_new,v,on=fields_to_merge,how="left")
-    FIPS_df = pd.merge(FIPS_df,v,on=fields_to_merge,how="left")
+    #New merge the new dfs to add the info
+    #FIPS_df_new = FIPS_df
+    for k,v in new_dfs.items():
+        fields_to_merge = [str(x) for x in state_and_county_fields[k]]
+        #FIPS_df_new = pd.merge(FIPS_df_new,v,on=fields_to_merge,how="left")
+        FIPS_df = pd.merge(FIPS_df,v,on=fields_to_merge,how="left")
 
-#combine state and county codes
-FIPS_df['FIPS'] = FIPS_df[state_and_county_fields["County"][0]] + FIPS_df[state_and_county_fields["County"][1]]
+    #combine state and county codes
+    FIPS_df['FIPS'] = FIPS_df[state_and_county_fields["County"][0]] + FIPS_df[state_and_county_fields["County"][1]]
 
-fields_to_keep = ["State","County","FIPS"]
-FIPS_df = FIPS_df[fields_to_keep]
+    fields_to_keep = ["State","County","FIPS"]
+    FIPS_df = FIPS_df[fields_to_keep]
 
-FIPS_df.to_csv(datapath+"FIPS.csv",index=False)
+    FIPS_df.to_csv(datapath+"FIPS.csv",index=False)
 
 
