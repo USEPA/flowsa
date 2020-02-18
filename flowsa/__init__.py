882265b2ae443d6d6d3be29af803e5c4e4429bf9
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-18 09:03:17 -0500

    Add bones of getFlowByActivity to init

---
+++ flowsa/__init__.py
@@ -0,0 +1,13 @@
+
+
+
+def getFlowByActivity(flowclass, datasource=None, year=None):
+    """
+    Retrieves stored data in the FlowByActivity format
+    :param flowclass: `Class' of the flow. required. E.g. 'Water'
+    :param datasource:
+    :param year:
+    :return: a pandas DataFrame in FlowByActivity format
+    """
+
+
