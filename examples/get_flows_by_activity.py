7cf42827c607b879fe79b5bb01a8dbd9641826aa
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-20 17:33:20 -0500

    Add a first function to get water flowbyactivity data

---
+++ examples/get_flows_by_activity.py
@@ -0,0 +1,10 @@
+# __init__.py (flowsa)
+# !/usr/bin/env python3
+# coding=utf-8
+# ingwersen.wesley@epa.gov
+"""
+Examples of use of flowsa
+"""
+import flowsa
+
+waterflowsbyactivity = flowsa.getFlowByActivity(flowclass='Water')
