8c13e0ad94586479b94ac9f351240428f9051973
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-20 17:24:50 -0500

    -Modify flowbyactivity to read sources_catalog to get names for all parquet files for a given flowclass - concat and return them

--- flowsa/__init__.py
+++ flowsa/__init__.py
@@ -1,5 +1,10 @@
-
-
+# __init__.py (flowsa)
+# !/usr/bin/env python3
+# coding=utf-8
+# ingwersen.wesley@epa.gov
+import yaml
+import pandas as pd
+from flowsa.common import outputpath, datapath, log
 
 def getFlowByActivity(flowclass, datasource=None, year=None):
     """
@@ -9,5 +14,24 @@ def getFlowByActivity(flowclass, datasource=None, year=None):
     :param year:
     :return: a pandas DataFrame in FlowByActivity format
     """
+    sources= datapath+'source_catalog.yaml'
+    with open(sources, 'r') as f:
+        config = yaml.safe_load(f)
+    source_dict_by_class = config['flowbyactivity']
+    try:
+        class_sources = source_dict_by_class[flowclass]
+    except KeyError:
+        log.error("No FlowByActivity data found for flow class "+flowclass)
+
+    class_flowbyactivity = pd.DataFrame()
+    for s in class_sources:
+        try:
+            flowbyactivity = pd.read_parquet(outputpath + s + ".parquet")
+            class_flowbyactivity = pd.concat([class_flowbyactivity,flowbyactivity], sort=False)
+        except FileNotFoundError:
+            log.error("No parquet file found for datasource "+ s + " in flowsa" )
+
+    return class_flowbyactivity
+
 
 
