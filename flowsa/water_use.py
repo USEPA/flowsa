8b5fab38b1a32f85fced03fbd3acde2f8a9d6a91
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-25 17:22:56 -0500

    Start laying out a water_use py to process water FlowByActivity data to generate FlowBySector

---
+++ flowsa/water_use.py
@@ -0,0 +1,31 @@
+"""
+Produces one or more FlowBySector files for the Water class
+"""
+import flowsa
+from flowsa.mapping import map_activities_to_sector
+
+wat_flowbyactivity = flowsa.getFlowByActivity('Water')
+
+# Removing records that we don't need
+
+
+
+# Unit conversion to m3
+
+
+##Map to FlowBySector
+#map_activities_to_sector(wat_flowbyactivity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
