c7eb18440b96a373d89fee2ce590b48de249d5a7
WesIngwersen <ingwersen.wesley@epa.gov>
2020-03-05 18:52:48 -0500

    Change getFlowByActivity to require year list and make it filter if datasource is provided

--- examples/get_flows_by_activity.py
+++ examples/get_flows_by_activity.py
@@ -7,4 +7,10 @@ Examples of use of flowsa
 """
 import flowsa
 
-waterflowsbyactivity = flowsa.getFlowByActivity(flowclass='Water')
+waterflowsbyactivity = flowsa.getFlowByActivity(flowclass='Water',years=[2010,2015])
+waterflowsbyactivity_2015 = flowsa.getFlowByActivity(flowclass='Water', years=[2015])
+employ_cbp_flowsbyactivity_2014 = flowsa.getFlowByActivity(flowclass='Employment', years=[2014],
+                                                     datasource="Census_CBP_EMP")
+
+
+
--- flowsa/__init__.py
+++ flowsa/__init__.py
@@ -1,17 +1,19 @@
 # __init__.py (flowsa)
 # !/usr/bin/env python3
 # coding=utf-8
-# ingwersen.wesley@epa.gov
+"""
+Public functions for flowsa
+"""
 import yaml
 import pandas as pd
 from flowsa.common import outputpath, datapath, log
 
-def getFlowByActivity(flowclass, datasource=None, year=None):
+def getFlowByActivity(flowclass, years, datasource=None):
     """
     Retrieves stored data in the FlowByActivity format
     :param flowclass: `Class' of the flow. required. E.g. 'Water'
-    :param datasource:
-    :param year:
+    :param year: list, a list of years [2015], or [2010,2011,2012]
+    :param datasource: str, the code of the datasource. If none returns data from all sources
     :return: a pandas DataFrame in FlowByActivity format
     """
     sources= datapath+'source_catalog.yaml'
@@ -19,17 +21,19 @@ def getFlowByActivity(flowclass, datasource=None, year=None):
         config = yaml.safe_load(f)
     source_dict_by_class = config['flowbyactivity']
     try:
-        class_sources = source_dict_by_class[flowclass]
+        sources = source_dict_by_class[flowclass]
     except KeyError:
         log.error("No FlowByActivity data found for flow class "+flowclass)
-
+    if datasource is not None:
+        sources = [datasource]
     class_flowbyactivity = pd.DataFrame()
-    for s in class_sources:
-        try:
-            flowbyactivity = pd.read_parquet(outputpath + s + ".parquet")
-            class_flowbyactivity = pd.concat([class_flowbyactivity,flowbyactivity], sort=False)
-        except FileNotFoundError:
-            log.error("No parquet file found for datasource "+ s + " in flowsa" )
+    for s in sources:
+        for y in years:
+            try:
+                flowbyactivity = pd.read_parquet(outputpath + s + "_" + str(y) + ".parquet")
+                class_flowbyactivity = pd.concat([class_flowbyactivity,flowbyactivity], sort=False)
+            except FileNotFoundError:
+                log.error("No parquet file found for datasource "+ s + "and year " + str(y) + " in flowsa" )
 
     return class_flowbyactivity
 
