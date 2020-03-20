7b32da3b5febc3a09fa402dcbcdd36f9dc234f55
melissagqc <jacob@gqc.com>
2020-03-20 14:28:35 -0400

    Refactor USDA and USGS build URLS

--- flowsa/USDA_CoA_Cropland.py
+++ flowsa/USDA_CoA_Cropland.py
@@ -16,6 +16,165 @@ def format_url_values(string):
         string = string.replace("&", "%26")
     return string
 
+def check_url_type(url_obj):
+    if type(url_obj) is list:
+        return "list"
+    elif type(url_obj) is str:
+        return "string"
+    elif type(url_obj) is dict:
+        return "dict"
+    else:
+        return "None of the above"
+
+def generate_url(url_order, create_url, type_urls):
+    if url_final_list == []:
+            if type(create_url) is str:
+                if url_order == "key":
+                    key_value = load_api_key(source)
+                    create_url = + create_url + "=" + key_value
+                url_final_list.append(format_url_values(create_url))
+            #YAMLS currently do not have a dictonary or list as the start of the URL. 
+            # Due to time conciderations this has not been tested.
+            # elif type(create_url) is list:
+            #     for u in create_url:
+            #         url_final_list.append(u)
+            # elif type(create_url) is dict:
+            #     dict_list = generateDict(create_url)
+            #     for d in dict_list:
+            #         url_final_list.append(d)
+    else:
+        if type(create_url) is list:
+            for i in range(len(url_final_list)):
+                url_previous = url_final_list[i]
+                for j in range(len(create_url)):
+                    url = url_previous + format_url_values(create_url[j]) + "/"
+                    if j == 0:
+                        url_final_list[i] = url
+                    else:
+                        url_final_list.append(url)
+        elif type(create_url) is str:
+            for i in range(len(url_final_list)):
+                if url_order == "key":
+                    key_value = load_api_key(source)
+                    create_url = create_url + "=" + key_value
+                url = url_final_list[i]
+                url = url + format_url_values(create_url)
+                url_final_list[i] = url
+        elif type(create_url) is dict:
+            dict_list = generate_dict(create_url, url_order)
+
+            for i in range(len(url_final_list)):
+                url_previous = url_final_list[i]
+                for j in range(len(dict_list)):
+                    if url_order == "url_params":
+                        url = url_previous + dict_list[j]
+                    else:
+                        url = url_previous + dict_list[j] + "/"
+                    
+                    if j == 0:
+                        url_final_list[i] = url
+                    else:
+                        url_final_list.append(url)
+    return url_final_list
+
+
+def generate_dict(dict_values, url_order):
+    dictonary_list = []
+    for k in dict_values:
+        if dictonary_list == []:
+            if type(dict_values[k]) is list:
+                if url_order == "url_params":
+                    for u in dict_values[k]:
+                        dictonary_list.append("&" + k + "=" + format_url_values(dict_values[k]))
+                else:
+                    for u in dict_values[k]:
+                        dictonary_list.append(format_url_values(dict_values[k]) + "/")
+            elif type(dict_values[k]) is str:
+                 if url_order == "url_params":
+                    dictonary_list.append("&" + k + "=" + format_url_values(dict_values[k]))
+                 else:
+                    dictonary_list.append(format_url_values(dict_values[k]) + "/")
+ 
+            #YAMLS currently do not have a dictonary of dictionaries but if we do it will involve recursion
+            #This is the start of this method. Due to time conciderations this has not been tested.
+            # elif type(d) is dict:
+            #     dict_list = generateDict(create_url)
+            #     for d in dict_list:
+            #         dictonary_list.append(d)
+        else:
+            if type(dict_values[k]) is list:
+                for i in range(len(dictonary_list)):
+                    previous_dictonary_list = dictonary_list[i]
+                    
+                    if url_order == "url_params":
+                        values_list = dict_values[k]
+                        for j in range(len(dict_values[k])):
+                            url = previous_dictonary_list + "&" + k + "=" + format_url_values(values_list[j])
+                            if j == 0:
+                                dictonary_list[i] = url
+                            else:
+                                dictonary_list.append(url)
+                    else:
+
+                        for j in range(len(dict_values[k])):
+                            url = previous_dictonary_list + format_url_values(values_list[j]) + "/"
+                            if j == 0:
+                                dictonary_list[i] = url
+                            else:
+                                dictonary_list.append(url)                        
+            elif type(dict_values[k]) is str:
+                for i in range(len(dictonary_list)):
+                    if url_order == "url_params":
+                        previous_dictonary_list = dictonary_list[i]
+                        dictonary_list[i] = previous_dictonary_list + "&" + k + "=" + dict_values[k]
+                    else:
+                        previous_dictonary_list = dictonary_list[i]
+                        dictonary_list[i] = previous_dictonary_list + dict_values[k] + "/"
+        #YAMLS currently do not have a dictonary of dictionaries but if we do it will involve recursion
+        #This is the start of this method. Due to time conciderations this has not been tested.
+            # elif type(d) is dict:
+            #     dict_list = generateDict(d)
+            #     for i in range(len(dictonary_list)):
+            #         url = dictonary_list[i]
+            #         for j in range(len(dict_list)):
+            #             url = url + dict_list[i]
+            #             if j == 0:
+            #                 dictonary_list[i] = url
+            #             else:
+            #                 dictonary_list.append(url)
+    return dictonary_list
+
+
+def build_url_list(config, source):
+    """
+    
+    :param config: 
+    :return: list of urls
+    """
+    for k,v in config.items():
+        if k == "url":
+            url_list = []
+            url_order = []
+            for g in v:
+                if g == "county":
+                    geo = v[g]
+                    for r in geo:
+                        if r == "url_order":
+                            url_order = geo[r]
+                            create_url = []
+                            type_urls =[]
+                            for p in url_order:
+                                create_url.append(geo[p])
+                                type_urls.append(check_url_type(geo[p]))
+                               
+            for i in range(len(url_order)):
+            #for d in url_order:
+                print(url_order[i], create_url[i], type_urls[i])
+                url_list = generate_url(url_order[i], create_url[i], type_urls[i])
+                for d in url_final_list:
+                    print(d)
+    return url_list
+
 def build_usda_crop_url_list(config):
     """
     
@@ -76,6 +235,16 @@ def call_usda_crop_urls(url_list):
 def check_value(value):
     if "(D)" in value:
         value = withdrawn_keyword
+    elif "," in value:
+        if "." in value:
+            value = value 
+        comma_split = value.split(",")
+        value_str = ""
+        for c in comma_split:
+            value_str = value_str + c
+        value = value_str
+    elif "." in value:
+        value = value 
     return value
 
 def parse_data(text):
@@ -101,41 +270,81 @@ def parse_data(text):
     max_list = []
 
     for d in data:
-        if "CROPS" in d["sector_desc"]:
-            flows_list = ["ACRES IN PRODUCTION", "OPERATIONS WITH AREA IN PRODUCTION", "ACRES HARVESTED", "OPERATIONS WITH AREA HARVESTED"]
-            des = str(d["short_desc"])
-            for fl in flows_list:
-                 if fl in des:
-                    class_list.append("Land")
-                    source_name_list.append(source)
-                    flow_name_list.append(str(d["statisticcat_desc"]))
-                    flow_amount_list.append(check_value(d["Value"]))
-                    unit_list.append(d["unit_desc"])
-                    activity_produced_list.append(None)
-                    activity_consumed_list.append(str(d["commodity_desc"]))
-                    measure_of_spread_list.append(check_value(d["CV (%)"]))
-                    spread_list.append(None)
-                    distribution_type_list.append(None)
-                    min_list.append(None)
-                    max_list.append(None)
-                    compartment_list.append(None)
-                    if d["agg_level_desc"] == "NATIONAL":
-                        fips_list.append(US_FIPS)
-                    else:
-                        if d["county_code"] == "":
-                            fips_list.append(str(d["state_fips_code"])+str("000"))
+            
+            stat_desc = ["AREA HARVESTED", "AREA IN PRODUCTION"]
+            do_desc = d["domain_desc"] 
+            if any(s in d["statisticcat_desc"] for s in stat_desc):
+                if (d["statisticcat_desc"] == "AREA HARVESTED" and (do_desc == "AREA HARVESTED" or do_desc == "TOTAL" or do_desc == "NAICS CLASSIFICATIONS")) or (d["statisticcat_desc"] == "AREA IN PRODUCTION" and (do_desc == "AREA IN PRODUCTION" or do_desc == "TOTAL")):
+                    if("fresh market" not in d["short_desc"].lower() and "processing" not in d["short_desc"].lower() and "irrigated, entire crop" not in d["short_desc"].lower() and "irrigated, none of crop" not in d["short_desc"].lower() and "irrigated, part of crop" not in d["short_desc"].lower()):
+                        class_list.append("Land")
+                        source_name_list.append(source)
+                        
+                        flow_amount_list.append(check_value(d["Value"]))
+                        if d["unit_desc"] == "OPERATIONS":
+                            unit_list.append("p")
+                            flow_name_list.append(str(d["unit_desc"]))
+                            activity_consumed_list.append(None)   
+                            if "-" in d["short_desc"] and "sunflower, non-oil type" not in d["short_desc"].lower():
+                                description = d["short_desc"].split("-")
+                                activity = description[0].strip()
+                                if "IRRIGATED" in activity:
+                                    activities = activity.split(", IRRIGATED")
+                                    activity_produced_list.append(activities[0] + activities[1])
+                                else:
+                                    activity_produced_list.append(activity)
+                            else:
+                                activity_produced_list.append(str(d["commodity_desc"]))
                         else:
-                            fips_list.append(str(d["state_fips_code"])+str(d["county_code"]))
-                    year_list.append(d["year"])
-                    data_reliability_list.append(None)
-                    data_collection_list.append(None)
-                    if "(" in d["domaincat_desc"]:
-                        domaincat_desc_split = d["domaincat_desc"].split("(")
-                        domaincat_desc = "(" + domaincat_desc_split[1]
-                        description_list.append(str(d["short_desc"]) + str(d["domain_desc"]) + domaincat_desc)
-                    else:
-                        description_list.append(str(d["short_desc"]) + str(d["domain_desc"]) + str(d["domaincat_desc"]))
-
+                            unit_list.append(d["unit_desc"])
+                            flow_name_list.append(str(d["statisticcat_desc"]))
+                            activity_produced_list.append(None)   
+                            if "-" in d["short_desc"] and "sunflower, non-oil type" not in d["short_desc"].lower():
+                                description = d["short_desc"].split("-")
+                                activity = description[0].strip()
+                                if "IRRIGATED" in activity:
+                                    activities = activity.split(", IRRIGATED")
+                                    activity_consumed_list.append(activities[0] + activities[1])
+                                else:
+                                    activity_consumed_list.append(activity)
+                            else:
+                                activity_consumed_list.append(str(d["commodity_desc"]))
+                           
+                        spread = check_value(d["CV (%)"])
+                        spread_list.append(spread)
+                        if spread == "W":
+                            measure_of_spread_list.append(None)
+                        else:
+                            measure_of_spread_list.append("RSD")
+                        distribution_type_list.append(None)
+                        min_list.append(None)
+                        max_list.append(None)
+                        
+                        if d["agg_level_desc"] == "NATIONAL":
+                            fips_list.append(US_FIPS)
+                        else:
+                            if d["county_code"] == "":
+                                fips_list.append(str(d["state_fips_code"])+str("000"))
+                            else:
+                                fips_list.append(str(d["state_fips_code"])+str(d["county_code"]))
+                        year_list.append(int(d["year"]))
+                        data_reliability_list.append(None)
+                        data_collection_list.append(2)
+                        compartment = ""
+                        if("IRRIGATED" in d["short_desc"]):
+                            compartment = "IRRIGATED " 
+                        
+                        if "(" in d["domaincat_desc"]:
+                            domaincat_desc_split = d["domaincat_desc"].split("(")
+                            domaincat_desc = "(" + domaincat_desc_split[1]
+                            compartment_list.append(compartment + domaincat_desc)
+                            description_list.append(str(d["short_desc"]) +" "+ str(d["domain_desc"]) +" "+ domaincat_desc)
+                        else:
+                            description_list.append(str(d["short_desc"]) +" "+ str(d["domain_desc"]) +" "+ str(d["domaincat_desc"]))
+                            if compartment == "":
+                                compartment_list.append(None)
+                            else:
+                                compartment_list.append(compartment.strip())
+                
     flow_by_activity = []
     for key in flow_by_activity_fields.keys():
         flow_by_activity.append(key)
@@ -162,7 +371,7 @@ def parse_data(text):
 
 if __name__ == '__main__':
     config = load_sourceconfig(source)
-    url_list = build_usda_crop_url_list(config)
+    url_list = build_url_list(config, source)
     df_lists = call_usda_crop_urls(url_list[0:52])
     
     for d in df_lists:
--- flowsa/datapull.py
+++ flowsa/datapull.py
@@ -2,28 +2,16 @@
 # !/usr/bin/env python3
 # coding=utf-8
 """
-Pulls data from data sources, reshapes them to flowbyactivity format, and saves to parquet
-Requires params for --year and --source
+Methods for pulling data from http sources
 """
-
-import argparse
 import yaml
 import requests
 import json
 from flowsa.common import outputpath, sourceconfigpath, log, local_storage_path,\
-     flow_by_activity_fields
-#from flowsa.USDA_CoA_Cropland import *
-#from flowsa.USGS_Water_Use import *
-#from flowsa.BLS_QCEW import *
-#from flowsa.Census_CBP import *
+      flow_by_activity_fields
 
-def parse_args():
-    #Make year a script parameter
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-y","--year", required=True, help="Year for data pull and save")
-    ap.add_argument("-s", "--source", required=True, help="Data source code to pull and save")
-    args = vars(ap.parse_args())
-    return args
+url_final_list = []
+
 
 def store_flowbyactivity(result, source, year=None):
     """Prints the data frame into a parquet file."""
@@ -37,6 +25,7 @@ def store_flowbyactivity(result, source, year=None):
         log.error('Failed to save '+source + "_" + str(year) +' file.')
 
 def make_http_request(url):
+    r = []
     try:
         r = requests.get(url)
     except requests.exceptions.ConnectionError:
@@ -64,6 +53,7 @@ def load_api_key(api_source):
     :return: the users API key as a string
     """
     keyfile = local_storage_path+'/'+ api_source + '_API_KEY.txt'
+    key = ""
     try:
         with open(keyfile,mode='r') as keyfilecontents:
             key = keyfilecontents.read()
@@ -97,14 +87,190 @@ def get_year_from_url(url):
         return None
 
 
-if __name__ == '__main__':
-    args = parse_args()
-    config = load_sourceconfig(args['source'])
 
-#    url_list = build_url_list(config)
-#    df_lists = call_urls(url_list)
-#    df = pd.concat(df_lists[d])
-#    log.info("Retrieved data for " + source + " " + d)
-#    df = reshape_to_flowbyactivity(df)
-#    df = store_metadata(config)
-#    store_flowbyactivity(df, source, d)
+def format_url_values(string):
+    if " " in string:
+        string = string.replace(" ", "%20")
+    if "&" in string:
+        string = string.replace("&", "%26")
+    return string
+
+def check_url_type(url_obj):
+    if type(url_obj) is list:
+        return "list"
+    elif type(url_obj) is str:
+        return "string"
+    elif type(url_obj) is dict:
+        return "dict"
+    else:
+        return "None of the above"
+
+def generate_url(url_order, create_url, type_urls, source):
+    if url_final_list == []:
+            if type(create_url) is str:
+                if url_order == "key":
+                    key_value = load_api_key(source)
+                    create_url = + create_url + "=" + key_value
+                url_final_list.append(format_url_values(create_url))
+            #YAMLS currently do not have a dictonary or list as the start of the URL. 
+            # Due to time conciderations this has not been tested.
+            # elif type(create_url) is list:
+            #     for u in create_url:
+            #         url_final_list.append(u)
+            # elif type(create_url) is dict:
+            #     dict_list = generateDict(create_url)
+            #     for d in dict_list:
+            #         url_final_list.append(d)
+    else:
+        if type(create_url) is list:
+            for i in range(len(url_final_list)):
+                url_previous = url_final_list[i]
+                for j in range(len(create_url)):
+                    url = url_previous + format_url_values(create_url[j]) + "/"
+                    if j == 0:
+                        url_final_list[i] = url
+                    else:
+                        url_final_list.append(url)
+        elif type(create_url) is str:
+            for i in range(len(url_final_list)):
+                if url_order == "key":
+                    key_value = load_api_key(source)
+                    create_url = create_url + "=" + key_value
+                url = url_final_list[i]
+                url = url + format_url_values(create_url)
+                url_final_list[i] = url
+        elif type(create_url) is dict:
+            dict_list = generate_dict(create_url, url_order)
+
+            for i in range(len(url_final_list)):
+                url_previous = url_final_list[i]
+                for j in range(len(dict_list)):
+                    if url_order == "url_params":
+                        url = url_previous + dict_list[j]
+                    else:
+                        url = url_previous + dict_list[j] + "/"
+                    
+                    if j == 0:
+                        url_final_list[i] = url
+                    else:
+                        url_final_list.append(url)
+    return url_final_list
+
+
+def generate_dict(dict_values, url_order):
+    dictonary_list = []
+    for k in dict_values:
+        if dictonary_list == []:
+            if type(dict_values[k]) is list:
+                if url_order == "url_params":
+                    for u in dict_values[k]:
+                        dictonary_list.append("&" + k + "=" + format_url_values(dict_values[k]))
+                else:
+                    for u in dict_values[k]:
+                        dictonary_list.append(format_url_values(dict_values[k]) + "/")
+            elif type(dict_values[k]) is str:
+                 if url_order == "url_params":
+                    dictonary_list.append("&" + k + "=" + format_url_values(dict_values[k]))
+                 else:
+                    dictonary_list.append(format_url_values(dict_values[k]) + "/")
+ 
+            #YAMLS currently do not have a dictonary of dictionaries but if we do it will involve recursion
+            #This is the start of this method. Due to time conciderations this has not been tested.
+            # elif type(d) is dict:
+            #     dict_list = generateDict(create_url)
+            #     for d in dict_list:
+            #         dictonary_list.append(d)
+        else:
+            if type(dict_values[k]) is list:
+                for i in range(len(dictonary_list)):
+                    previous_dictonary_list = dictonary_list[i]
+                    
+                    if url_order == "url_params":
+                        values_list = dict_values[k]
+                        for j in range(len(dict_values[k])):
+                            url = previous_dictonary_list + "&" + k + "=" + format_url_values(values_list[j])
+                            if j == 0:
+                                dictonary_list[i] = url
+                            else:
+                                dictonary_list.append(url)
+                    else:
+
+                        for j in range(len(dict_values[k])):
+                            url = previous_dictonary_list + format_url_values(values_list[j]) + "/"
+                            if j == 0:
+                                dictonary_list[i] = url
+                            else:
+                                dictonary_list.append(url)                        
+            elif type(dict_values[k]) is str:
+                for i in range(len(dictonary_list)):
+                    if url_order == "url_params":
+                        previous_dictonary_list = dictonary_list[i]
+                        dictonary_list[i] = previous_dictonary_list + "&" + k + "=" + dict_values[k]
+                    else:
+                        previous_dictonary_list = dictonary_list[i]
+                        dictonary_list[i] = previous_dictonary_list + dict_values[k] + "/"
+        #YAMLS currently do not have a dictonary of dictionaries but if we do it will involve recursion
+        #This is the start of this method. Due to time conciderations this has not been tested.
+            # elif type(d) is dict:
+            #     dict_list = generateDict(d)
+            #     for i in range(len(dictonary_list)):
+            #         url = dictonary_list[i]
+            #         for j in range(len(dict_list)):
+            #             url = url + dict_list[i]
+            #             if j == 0:
+            #                 dictonary_list[i] = url
+            #             else:
+            #                 dictonary_list.append(url)
+    return dictonary_list
+
+
+def build_url_list(config, source):
+    """
+    
+    :param config: 
+    :return: list of urls
+    """
+    for k,v in config.items():
+        api_source = ""
+        if k == "api_name":
+            api_source = v
+        else:
+            api_source = source
+        if k == "url":
+            url_list = []
+            url_order = []
+            for g in v:
+                if g == "county":
+                    geo = v[g]
+                    for r in geo:
+                        if r == "url_order":
+                            url_order = geo[r]
+                            create_url = []
+                            type_urls =[]
+                            for p in url_order:
+                                create_url.append(geo[p])
+                                type_urls.append(check_url_type(geo[p]))
+                    elif g == "state":
+                    geo = v[g]
+                    for r in geo:
+                        if r == "url_order":
+                            url_order = geo[r]
+                            create_url = []
+                            type_urls =[]
+                            for p in url_order:
+                                create_url.append(geo[p])
+                                type_urls.append(check_url_type(geo[p]))
+                    elif g == "national":
+                        geo = v[g]
+                    for r in geo:
+                        if r == "url_order":
+                            url_order = geo[r]
+                            create_url = []
+                            type_urls =[]
+                            for p in url_order:
+                                create_url.append(geo[p])
+                                type_urls.append(check_url_type(geo[p]))
+                               
+            for i in range(len(url_order)):
+                url_list = generate_url(url_order[i], create_url[i], type_urls[i], api_source)
+    return url_list
