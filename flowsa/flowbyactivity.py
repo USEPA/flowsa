2829227fa65e7fcdac2208255dbc311b2d085748
WesIngwersen <ingwersen.wesley@epa.gov>
2020-03-23 09:05:20 -0400

    -Flesh out agg_by_geoscale function

--- flowsa/flowbyactivity.py
+++ flowsa/flowbyactivity.py
@@ -2,9 +2,18 @@
 Helper functions for flowbyactivity data
 """
 import numpy as np
-from flowsa.common import log, get_county_FIPS, get_state_FIPS, US_FIPS
+from flowsa.common import log, get_county_FIPS, get_state_FIPS, US_FIPS, activity_fields
 
-def filter_by_geographic_scale(flowbyactivity_df, geoscale):
+
+fba_activity_fields = [activity_fields['ProducedBy'][0]['flowbyactivity'],
+                       activity_fields['ConsumedBy'][0]['flowbyactivity']]
+
+fba_default_grouping_fields = ['FlowName','Unit',
+                               'ActivityProducedBy',
+                               'ActivityConsumedBy',
+                               'Compartment', 'FIPS', 'Year']
+
+def filter_by_geoscale(flowbyactivity_df, geoscale):
     """
     Filter flowbyactivity by FIPS at the given scale
     :param flowbyactitvy_df:
@@ -36,6 +45,24 @@ def agg_by_geoscale(flowbyactivity_df, from_scale, to_scale):
     :param to_scale:
     :return:
     """
+    from flowsa.common import fips_number_key
+    from_scale_dig = fips_number_key[from_scale]
+    to_scale_dig = fips_number_key[to_scale]
+
+    #use from scale to filter by these values
+    fba_from_scale = filter_by_geoscale(flowbyactivity_df,from_scale)
+
+    group_cols = fba_default_grouping_fields.copy()
+    group_cols.remove('FIPS')
+    if to_scale == 'state':
+        fba_from_scale['to_FIPS'] = fba_from_scale['FIPS'].apply(lambda x: str(x[0:2]))
+        group_cols.append('to_FIPS')
+    #if national no need to do anything
+    #breaking here
+    fba_agg = aggregator(fba_from_scale,group_cols)
+    return fba_agg
+
+
 
 def aggregator(flowbyactivity_df, groupbycols):
     """
@@ -45,11 +72,12 @@ def aggregator(flowbyactivity_df, groupbycols):
     :return:
     """
 
-    wm = lambda x: np.average(x, weights=flowbyactivity_df.loc[x.index, "FlowAmount"]
-    )
-    flowbyactivity_df = flowbyactivity_df.groupby(groupbycols, as_index=False).agg({"FlowAmount":"sum",
-                                                                    "DataReliability": wm,
-                                                                   "DataCollection": wm})
-    return flowbyactivity_df
+    wm = lambda x: np.average(x, weights=flowbyactivity_df.loc[x.index, "FlowAmount"])
+    flowbyactivity_dfg = flowbyactivity_df.groupby(groupbycols, as_index=False).agg({"FlowAmount":"sum"})
+
+    #flowbyactivity_dfg = flowbyactivity_df.groupby(groupbycols).agg({"FlowAmount":"sum",
+    #                                                                "DataReliability": wm,
+    #                                                               "DataCollection": wm})
+    return flowbyactivity_dfg
 
 
