a243cb3fd15260e118637848142726dcfb9bcaf1
WesIngwersen <ingwersen.wesley@epa.gov>
2020-03-28 08:07:55 -0400

    minor change adding fba_fill_na_dict

--- flowsa/flowbysector.py
+++ flowsa/flowbysector.py
@@ -8,7 +8,8 @@ import flowsa
 import yaml
 from flowsa.common import log, flowbyactivitymethodpath
 from flowsa.mapping import add_sectors_to_flowbyactivity
-from flowsa.flowbyactivity import filter_by_geoscale, fba_activity_fields, agg_by_geoscale,create_fill_na_dict
+from flowsa.flowbyactivity import fba_activity_fields, agg_by_geoscale, \
+    fba_fill_na_dict
 
 
 def load_method(method_name):
@@ -30,9 +31,9 @@ def load_method(method_name):
 
 #Need to create sums by flowtype and unit to check against later
 
-#Add in Sector matches now to producing and consuming activities
-#df = add_sectors_to_flowbyactivity(df)
-#Very important to note that values have not been allocated but are repeated at this point
+# Add in Sector matches now to producing and consuming activities
+# df = add_sectors_to_flowbyactivity(df)
+# Very important to note that values have not been allocated but are repeated at this point
 
 
 # Removing records that we don't need
@@ -51,20 +52,19 @@ def main(method_name):
         flows = flowsa.getFlowByActivity(flowclass=v['class'],
                                                     years=[v['year']],
                                                     datasource = k)
-        #drop description field
+        # drop description field
         flows = flows.drop(columns='Description')
-        #fill null values
-        fill_dict = create_fill_na_dict()
-        flows = flows.fillna(value=fill_dict)
+        # fill null values
+        flows = flows.fillna(value=fba_fill_na_dict)
         activities = v['activity_sets']
         for aset,attr in activities.items():
             # subset by named activities
             names = [attr['names']]
             flows = flows[(flows[fba_activity_fields[0]].isin(names)) |
                           (flows[fba_activity_fields[1]].isin(names))]
-            #Reset index values after subset
+            # Reset index values after subset
             flows = flows.reset_index()
-            #aggregate geographically
+            # aggregate geographically
             from_scale = v['geoscale_to_use']
             to_scale = method['target_geoscale']
             flows = agg_by_geoscale(flows, from_scale, to_scale)
