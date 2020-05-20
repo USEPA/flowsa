2e76974652d7fc1bfd659bf0d84d225038ece3f6
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-05-20 18:11:36 -0400

    updated pop code(different urls for 13 -14). added parquets for 13 - 18

--- flowsa/Census_PEP_Population.py
+++ flowsa/Census_PEP_Population.py
@@ -13,28 +13,57 @@ This script is designed to run with a configuration parameter
 
 import pandas as pd
 import json
-from flowsa.common import US_FIPS, load_api_key
+from flowsa.common import US_FIPS, load_api_key, get_all_state_FIPS_2
 
 
 def Census_pop_URL_helper(build_url, config, args):
     urls = []
 
+    # get date code for july 1 population numbers
+    for k, v in config['datecodes'].items():
+        if str(args['year']) == str(k):
+            dc = str(v)
+
     # the url for 2010 and earlier is different
     url2000 = 'https://api.census.gov/data/2000/pep/int_population?get=POP,DATE_DESC&for=__aggLevel__:*&DATE_=12&key=__apiKey__'
 
+    # state fips required for county level 13-14
+    FIPS_2 = get_all_state_FIPS_2()['FIPS_2']
+    # drop puerto rico
+    FIPS_2 = FIPS_2[FIPS_2 != '72']
+
     for c in config['agg_levels']:
-        if args['year'] > '2010':
-            url = build_url
-            url = url.replace("__aggLevel__", c)
-            urls.append(url)
-        elif args['year'] == '2010':
-            url = url2000
-            url = url.replace("__aggLevel__", c)
-            if c == "us":
-                url = url.replace("*", "1")
-            userAPIKey = load_api_key(config['api_name'])  # (common.py fxn)
-            url = url.replace("__apiKey__", userAPIKey)
-            urls.append(url)
+        # this timeframe requires state fips at the county level in the url
+        if '2010' < args['year'] < '2015' and c == 'county':
+            for b in FIPS_2:
+                url = build_url
+                url = url.replace("__aggLevel__", c)
+                url = url.replace("__DateCode__", dc)
+                url = url.replace("population?&", 'cty?')
+                url = url.replace("DATE_CODE", 'DATE_')
+                url = url.replace("&key", "&in=state:" + b + "&key")
+                urls.append(url)
+        else:
+            if args['year'] > '2010':
+                url = build_url
+                url = url.replace("__aggLevel__", c)
+                url = url.replace("__DateCode__", dc)
+                # url date variable different pre 2018
+                if args['year'] < '2018':
+                    url = url.replace("DATE_CODE", 'DATE_')
+                # url for 2011 - 2014 slightly modified
+                if args['year'] < '2015' and c != 'county':
+                    url = url.replace("population?&", 'natstprc?')
+                urls.append(url)
+            elif args['year'] == '2010':
+                url = url2000
+                url = url.replace("__aggLevel__", c)
+                url = url.replace("&in=state:__stateFIPS__", '')
+                if c == "us":
+                    url = url.replace("*", "1")
+                userAPIKey = load_api_key(config['api_name'])  # (common.py fxn)
+                url = url.replace("__apiKey__", userAPIKey)
+                urls.append(url)
     return urls
 
 
@@ -50,6 +79,8 @@ def census_pop_parse(dataframe_list, args):
     df = pd.concat(dataframe_list, sort=False)
     # Add year
     df['Year'] = args["year"]
+    # drop puerto rico
+    df = df[df['state'] != '72']
     # replace null county cells with '000'
     df['county'] = df['county'].fillna('000')
     # Make FIPS as a combo of state and county codes
@@ -77,5 +108,8 @@ def census_pop_parse(dataframe_list, args):
     # temporary data quality scores
     df['DataReliability'] = None
     df['DataCollection'] = None
+    # sort df
+    df = df.sort_values(['Location'])
+    # reset index
+    df.reset_index(drop=True, inplace=True)
     return df
-
