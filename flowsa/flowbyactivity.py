dde353340e8d5fe4a4704e78b690a5ae59e2a140
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-06-04 13:24:55 -0400

    updated agg by geoscale fxn to be naics based

--- flowsa/flowbyactivity.py
+++ flowsa/flowbyactivity.py
@@ -5,6 +5,7 @@ import flowsa
 from functools import reduce
 import numpy as np
 import pandas as pd
+from flowsa.mapping import get_activitytosector_mapping
 from flowsa.common import log, get_county_FIPS, get_state_FIPS, US_FIPS, activity_fields, \
     flow_by_activity_fields, load_sector_crosswalk, sector_source_name, datapath
 
@@ -109,6 +110,7 @@ def aggregator(flowbyactivity_df, groupbycols):
                 "Spread":wm,
                 "DataReliability": wm,
                 "DataCollection": wm}
+
     flowbyactivity_dfg = flowbyactivity_df.groupby(groupbycols, as_index=False).agg(agg_funx)
     return flowbyactivity_dfg
 
@@ -186,13 +188,14 @@ def convert_unit(df):
     return df
 
 
+
 def geoscale_flow_comparison(flowclass, years, datasource, activitynames='all', to_scale='national'):
     """ Aggregates county data to state and national, and state data to national level, allowing for comparisons
-        in flow totals for a given flowclass and industry. First assigns all flownames to NAICS and standardizes units"""
+        in flow totals for a given flowclass and industry. First assigns all flownames to NAICS and standardizes units.
 
-    # problems with the code:
-    # will not work for usgs wu because nat'l level data has different names. only works for datasets that have county
-    # level data
+        Assigned to NAICS rather than using FlowNames for aggregation to negate any changes in flownames across
+        time/geoscale
+    """
 
     # load parquet file checking aggregation
     flows = flowsa.getFlowByActivity(flowclass=flowclass,
@@ -214,6 +217,19 @@ def geoscale_flow_comparison(flowclass, years, datasource, activitynames='all', 
     # Reset index values after subset
     flow_subset = flow_subset.reset_index()
 
+    # pull naics crosswalk
+    mapping = get_activitytosector_mapping(flow_subset['SourceName'].all())
+
+    # assign naics to activities
+    flow_subset = pd.merge(flow_subset, mapping[['Activity', 'Sector']], left_on='ActivityProducedBy',
+                           right_on='Activity', how='left').rename({'Sector': 'SectorProducedBy'}, axis=1)
+    flow_subset = pd.merge(flow_subset, mapping[['Activity', 'Sector']], left_on='ActivityConsumedBy',
+                           right_on='Activity', how='left').rename({'Sector': 'SectorConsumedBy'}, axis=1)
+    flow_subset = flow_subset.drop(columns=['ActivityProducedBy', 'ActivityConsumedBy', 'Activity_x', 'Activity_y',
+                                            'Description'])
+    flow_subset['SectorProducedBy'] = flow_subset['SectorProducedBy'].replace({np.nan: None}).astype(str)
+    flow_subset['SectorConsumedBy'] = flow_subset['SectorConsumedBy'].replace({np.nan: None}).astype(str)
+
     # create list of geoscales for aggregation
     if to_scale == 'national':
         geoscales = ['national', 'state', 'county']
@@ -223,41 +239,55 @@ def geoscale_flow_comparison(flowclass, years, datasource, activitynames='all', 
     # create empty df list
     flow_dfs = []
     for i in geoscales:
-        # filter by geoscale
-        fba_from_scale = filter_by_geoscale(flow_subset, i)
+        try:
+            # filter by geoscale
+            fba_from_scale = filter_by_geoscale(flow_subset, i)
 
-        # remove "location" as a column
-        group_cols = fba_default_grouping_fields.copy()
-        group_cols.remove('Location')
+            # remove/add column names as a column
+            group_cols = fba_default_grouping_fields.copy()
+            for i in ['Location', 'ActivityProducedBy', 'ActivityConsumedBy']:
+                group_cols.remove(i)
+            for i in ['SectorProducedBy', 'SectorConsumedBy']:
+                group_cols.append(i)
 
-        # county sums to state and national, state sums to national
-        if to_scale == 'state':
-            fba_from_scale['to_Location'] = fba_from_scale['Location'].apply(lambda x: str(x[0:2]))
-        elif to_scale == 'national':
-            fba_from_scale['to_Location'] = US_FIPS
-        group_cols.append('to_Location')
+            # county sums to state and national, state sums to national
+            if to_scale == 'state':
+                fba_from_scale['to_Location'] = fba_from_scale['Location'].apply(lambda x: str(x[0:2]))
+            elif to_scale == 'national':
+                fba_from_scale['to_Location'] = US_FIPS
+            group_cols.append('to_Location')
 
-        # aggregate
-        fba_agg = aggregator(fba_from_scale, group_cols)
+            # aggregate
+            fba_agg = aggregator(fba_from_scale, group_cols)
 
-        # rename flowamount column, based on geoscale
-        fba_agg = fba_agg.rename(columns={"FlowAmount": "FlowAmount_" + i})
+            # rename flowamount column, based on geoscale
+            fba_agg = fba_agg.rename(columns={"FlowAmount": "FlowAmount_" + i})
 
-        # drop fields irrelevant to aggregated flow comparision
-        drop_fields = flows[['MeasureofSpread', 'Spread', 'DistributionType', 'DataReliability','DataCollection']]
-        fba_agg = fba_agg.drop(columns=drop_fields)
+            # drop fields irrelevant to aggregated flow comparision
+            drop_fields = flows[['MeasureofSpread', 'Spread', 'DistributionType', 'DataReliability','DataCollection']]
+            fba_agg = fba_agg.drop(columns=drop_fields)
 
-        # reset index
-        fba_agg = fba_agg.reset_index(drop=True)
+            # reset index
+            fba_agg = fba_agg.reset_index(drop=True)
 
-        #append to datatframe
-        flow_dfs.append(fba_agg)
+            flow_dfs.append(fba_agg)
+        except:
+            pass
 
     # merge list of dfs by column
     flow_comparison = reduce(lambda left, right: pd.merge(left, right, on=['Class', 'SourceName', 'FlowName', 'Unit',
-                                                                           'ActivityProducedBy','ActivityConsumedBy',
+                                                                           'SectorProducedBy', 'SectorConsumedBy',
                                                                            'Compartment', 'to_Location',
                                                                            'LocationSystem', 'Year']), flow_dfs)
 
     return flow_comparison
+
+
+
+
+
+
+
+
+
 
