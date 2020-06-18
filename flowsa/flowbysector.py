bf63187302fb3cc3ad0d7c2e235a8af779d6e441
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-06-18 13:01:56 -0400

    aggregation by sector function

--- flowsa/flowbysector.py
+++ flowsa/flowbysector.py
@@ -13,7 +13,7 @@ from flowsa.common import log, flowbyactivitymethodpath, datapath, flow_by_secto
 from flowsa.mapping import add_sectors_to_flowbyactivity
 from flowsa.flowbyactivity import fba_activity_fields, agg_by_geoscale, \
     fba_fill_na_dict, convert_unit, activity_fields, fba_default_grouping_fields, \
-    get_fba_allocation_subset, add_missing_flow_by_fields, fbs_activity_fields
+    get_fba_allocation_subset, add_missing_flow_by_fields, fbs_activity_fields, aggregator
 
 
 fbs_fill_na_dict = create_fill_na_dict(flow_by_sector_fields)
@@ -38,6 +38,16 @@ def load_method(method_name):
 
 def allocate_by_sector(fba_w_sectors, allocation_method):
 
+    # testing purposes
+    fba_w_sectors = fba_allocation_subset.copy()
+    allocation_method = attr['allocation_method']
+
+    # drop any columns that contain a "-" in sector column
+    fba_w_sectors = fba_w_sectors[~fba_w_sectors['Sector'].str.contains('-', regex=True)]
+
+    # run sector aggregation fxn to determine total flowamount for each level of sector
+    fba_w_sectors = sector_aggregation(fba_w_sectors)
+
     # create column of naics length to help with allocation grouping
     fba_w_sectors['NaicsLength'] = fba_w_sectors['Sector'].apply(lambda x: len(x))
 
@@ -54,6 +64,52 @@ def allocate_by_sector(fba_w_sectors, allocation_method):
         allocation_df = pd.concat(allocations)
 
         return allocation_df
+
+
+def sector_aggregation(fbs_df):
+    """
+    Function that checks if a sector aggregation exists, and if not, sums the less aggregated sector
+    :param fbs_df: flow by sector dataframe
+    :return:
+    """
+
+    # group by columns
+    group_cols= fba_default_grouping_fields
+    group_cols = [e for e in group_cols if e not in ('ActivityProducedBy', 'ActivityConsumedBy')]
+    group_cols.append('Sector')
+
+    # find the longest length naics (will be 6 or 8), needs to be integer for for loop
+    length = max(fbs_df['Sector'].apply(lambda x: len(x)).unique())
+    # for loop in reverse order longest length naics minus 1 to 2
+    # appends missing naics levels to df
+    for i in range(length-1, 1, -1):
+        # subset df to sectors with length = i and length = i + 1
+        df_subset = fbs_df.loc[fbs_df['Sector'].apply(lambda x: i + 2 > len(x) >= i)]
+        # create a list of i digit sectors in df subset
+        sector_list = df_subset['Sector'].apply(lambda x: str(x[0:i])).unique().tolist()
+        # create a list of sectors that are exactly i digits long
+        existing_sectors = df_subset['Sector'].loc[df_subset['Sector'].apply(lambda x: len(x) == i)].unique().tolist()
+        # list of sectors of length i that are not in sector list
+        missing_sectors = np.setdiff1d(sector_list, existing_sectors).tolist()
+        # add start of symbol to missing list
+        missing_sectors = ["^" + e for e in missing_sectors]
+        if len(missing_sectors) != 0:
+            # new df of sectors that start with missing sectors. drop the last digit of the sector and sum flow amounts
+            agg_sectors = df_subset.loc[df_subset['Sector'].str.contains('|'.join(missing_sectors))]
+            # only keep data with length greater than i
+            agg_sectors = agg_sectors.loc[agg_sectors['Sector'].apply(lambda x: len(x) > i)]
+            agg_sectors['Sector'] = agg_sectors['Sector'].apply(lambda x: str(x[0:i]))
+            agg_sectors = agg_sectors.fillna(0).reset_index()
+            # aggregate the new sector flow amounts
+            agg_sectors = aggregator(agg_sectors, group_cols)
+            agg_sectors = agg_sectors.fillna(0).reset_index(drop=True)
+            # append to df
+            fbs_df = fbs_df.append(agg_sectors, sort=True)
+
+    # sort df
+    fbs_df = fbs_df.sort_values(['Location', 'Sector'])
+
+    return fbs_df
 
 
 def store_flowbysector(fbs_df, parquet_name):
@@ -141,6 +197,8 @@ def main(method_name):
                 fba_allocation_subset = fba_allocation_subset.reset_index(drop=True)
                 # generalize activity field names to enable link to water withdrawal table
                 fba_allocation_subset = generalize_activity_field_names(fba_allocation_subset)
+                # drop activity column
+                fba_allocation_subset = fba_allocation_subset.drop(columns=['Activity', 'Description', 'Min', 'Max'])
                 # create flow allocation ratios
                 flow_allocation = allocate_by_sector(fba_allocation_subset, attr['allocation_method'])
 
