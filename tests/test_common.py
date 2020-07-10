46fee6f631bb3a8e490b623d8d5b556ef5b096b6
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-07-10 16:20:05 -0400

    add files for test runs

--- tests/test_common.py
+++ tests/test_common.py
@@ -4,44 +4,70 @@
 
 """ save flowbyactivity test data as parquet files """
 
-import sys
-import os
-import yaml
-import requests
-import pandas as pd
-import numpy as np
-import logging as log
-import appdirs
+from flowsa.common import *
 
-log.basicConfig(level=log.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S', stream=sys.stdout)
-try:
-    testmodulepath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
-except NameError:
-    testmodulepath = 'flowsa/'
-
-
-testdatapath = testmodulepath + 'tests/data/'
-testsourceconfigpath = testdatapath + 'tests/sourceconfig/'
-testoutputpath = testmodulepath + 'tests/output/'
-testfbaoutputpath = testoutputpath + 'FlowByActivity/'
-testfbsoutputpath = testoutputpath + 'FlowBySector/'
-testflowbyactivitymethodpath = testdatapath + 'flowbysectormethods/'
 
 def store_test_flowbyactivity(csvname, year=None):
     """Prints the data frame into a parquet file."""
     if year is not None:
-        f = testfbaoutputpath + csvname + "_" + str(year) + '.parquet'
+        f = fbaoutputpath + csvname + "_" + str(year) + '.parquet'
     else:
-        f = testfbaoutputpath + csvname+ '.parquet'
+        f = fbaoutputpath + csvname + '.parquet'
     try:
-        result = pd.read_csv(testdatapath + csvname + '.csv', dtype='str')
+        result = pd.read_csv(datapath + csvname + '.csv', dtype='str')
+        result = add_missing_flow_by_fields(result, flow_by_activity_fields)
         result.to_parquet(f, engine="pyarrow")
     except:
-        log.error('Failed to save '+ csvname + "_" + str(year) +' file.')
+        log.error('Failed to save ' + csvname + "_" + str(year) + ' file.')
+
+
+def gettestFlowByActivity(flowclass, years, datasource):
+    """
+    Retrieves stored data in the FlowByActivity format
+    :param flowclass: list, a list of`Class' of the flow. required. E.g. ['Water'] or
+     ['Land', 'Other']
+    :param year: list, a list of years [2015], or [2010,2011,2012]
+    :param datasource: str, the code of the datasource.
+    :return: a pandas DataFrame in FlowByActivity format
+    """
+    fbas = pd.DataFrame()
+    for y in years:
+        try:
+            fba = pd.read_parquet(fbaoutputpath + datasource + "_" + str(y) + ".parquet")
+            fba = fba[fba['Class'].isin(flowclass)]
+            fbas = pd.concat([fbas, fba], sort=False)
+        except FileNotFoundError:
+            log.error("No parquet file found for datasource " + datasource + "and year " + str(
+                y) + " in flowsa")
+    return fbas
+
+
+def gettestFlowBySector(methodname):
+    """
+    Retrieves stored data in the FlowBySector format
+    :param methodname: string, Name of an available method for the given class
+    :return: dataframe in flow by sector format
+    """
+    fbs = pd.DataFrame()
+    try:
+        fbs = pd.read_parquet(fbsoutputpath + methodname + ".parquet")
+    except FileNotFoundError:
+        log.error("No parquet file found for datasource " + methodname + " in flowsa")
+    return fbs
 
 
 # store csv test data as parquet files
-store_test_flowbyactivity('test_dataset_1_aquaculture', '2015')
-store_test_flowbyactivity('test_dataset_2_irrigation_crop', '2015')
+store_test_flowbyactivity('test_dataset_1', '2015')
+store_test_flowbyactivity('test_dataset_2', '2015')
 # store_test_flowbyactivity('test_dataset_3', '2015')
+
+# read test fba parquets
+test_1 = gettestFlowByActivity(flowclass=['Water'], years=[2015],
+                                                      datasource="test_dataset_1")
+test_2 = gettestFlowByActivity(flowclass=['Water'], years=[2015], datasource="test_dataset_2")
+
+test_1_fbs = gettestFlowBySector('test_dataset_1_2015')
+test_2_fbs = gettestFlowBySector('test_dataset_2_2015')
+
+
+method_name = 'test_dataset_2_2015'
