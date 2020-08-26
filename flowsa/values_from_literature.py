31b6580e2e08bee61122ba9c53d1031cd182acc2
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-08-26 15:04:04 -0400

    created python file to house and document values found in literature

---
+++ flowsa/values_from_literature.py
@@ -0,0 +1,22 @@
+# values_from_literature.py (flowsa)
+# !/usr/bin/env python3
+# coding=utf-8
+
+"""
+Values from the literature used for data allocation are specified here and can be called on using functions.
+"""
+
+
+def get_US_urban_green_space_and_public_parks_ratio():
+    """
+    10.7% of the total urban area is accounted for by urban green space and public parks
+
+    Larson LR, Jennings V, Cloutier SA (2016) Public Parks and Wellbeing in Urban Areas of the United States.
+    PLoS ONE 11(4): e0153211. https://doi.org/10.1371/journal.pone.0153211
+    """
+
+    value = 0.107
+
+    return value
+
+
