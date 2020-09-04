36e7958f78d71e575dd2f182ffe6b69d214111fe
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-09-04 16:19:47 -0400

    add BEA crosswalk

---
+++ scripts/write_Crosswalk_BEA_from_useeior.py
@@ -0,0 +1,17 @@
+# write_Crosswalk_BLS_QCEW.py (scripts)
+# !/usr/bin/env python3
+# coding=utf-8
+
+"""
+The BEA to NAICS Crosswalk was pulled from USEEIOR's MastereCrosswalk2012.rda on 09/04/2020.
+
+The original file is found here:
+https://github.com/USEPA/useeior/blob/master/data/MasterCrosswalk2012.rda
+
+csv obtained by running the following code in Rstudio:
+cw  <- load('MasterCrosswalk2012.rda')
+write.csv(get(cw), file='Crosswalk_BEA_toNAICs.csv')
+
+CSV manually added to flowsa as Crosswalk_BEA_toNAICS_raw.csv
+
+"""
