cbbf8c11d6e989841080e82fc733f973877552af
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-09-16 18:27:52 -0400

    stopped dropping naics in expanded naics mapping

--- flowsa/mapping.py
+++ flowsa/mapping.py
@@ -110,14 +110,11 @@ def expand_naics_list(df, sectorsourcename):
     # create list of sectors that exist in original df, which, if created when expanding sector list cannot be added
     existing_sectors = df[['Sector']]
     existing_sectors = existing_sectors.drop_duplicates()
-    
+
     naics_df = pd.DataFrame([])
     for i in existing_sectors['Sector']:
         dig = len(str(i))
         n = sectors.loc[sectors[sectorsourcename].apply(lambda x: x[0:dig]) == i]
-        # drop any rows in n that contain sectors already in original df (if sector length is longer)
-        existing_sectors_subset = existing_sectors.loc[existing_sectors['Sector'].apply(lambda x: len(str(x)) > dig)]
-        n = n[~n[sectorsourcename].isin(existing_sectors_subset['Sector'].tolist())]
         if len(n) != 0:
             n.loc[:, 'Sector'] = i
             naics_df = naics_df.append(n)
