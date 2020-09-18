841393d3dff66e1123dec9a43282c457d263dd96
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-09-17 23:48:58 -0400

    mapped nonexistent naics-like bls activities to 6 digit naics

--- scripts/write_Crosswalk_BLS_QCEW.py
+++ scripts/write_Crosswalk_BLS_QCEW.py
@@ -7,7 +7,9 @@
 Create a crosswalk for BLS QCEW to NAICS 2012. Downloaded data is already provided in NAICS
 """
 import pandas as pd
+import numpy as np
 from flowsa.common import datapath, fbaoutputpath
+from flowsa.common import load_sector_length_crosswalk
 
 def unique_activity_names(datasource, years):
     """read in the ers parquet files, select the unique activity names"""
@@ -22,6 +24,31 @@ def unique_activity_names(datasource, years):
     df = df.drop_duplicates()
     return df
 
+def link_non_bls_naics_to_naics(df):
+    """
+    BLS has 6 digit naics that are not recognized by Census. Map bls data to naics
+    :param df:
+    :return:
+    """
+
+    # load sector crosswalk
+    cw = load_sector_length_crosswalk()
+    cw_sub = cw[['NAICS_6']].drop_duplicates()
+
+    # subset the df to the 6 digit naics
+    df_sub = df[df['Activity'].apply(lambda x: len(x) == 6)]
+
+    # create a list of 6 digit bls activities that are not naics
+    unmapped_df = pd.merge(df_sub, cw_sub, indicator=True, how='outer', left_on='Activity',
+                           right_on='NAICS_6').query('_merge=="left_only"').drop('_merge', axis=1).reset_index(drop=True)
+    act_list = unmapped_df['Activity'].values.tolist()
+
+    # if in the activity list, the sector should be modified so last digit is 0
+
+    df['Sector'] = np.where(df['Activity'].isin(act_list), df['Activity'].apply(lambda x: x[0:5]) + '0', df['Sector'])
+
+    return df
+
 
 if __name__ == '__main__':
     # select years to pull unique activity names
@@ -34,6 +61,7 @@ if __name__ == '__main__':
     df.loc[df['Activity'] == '31-33', 'Sector'] = '31'
     df = df.append(pd.DataFrame([['BLS_QCEW', '31-33', '32']], columns=['ActivitySourceName', 'Activity', 'Sector']))
     df = df.append(pd.DataFrame([['BLS_QCEW', '31-33', '33']], columns=['ActivitySourceName', 'Activity', 'Sector']))
+    df = link_non_bls_naics_to_naics(df)
     # Add additional columns
     df['SectorSourceName'] = "NAICS_2012_Code"
     df['SectorType'] = "I"
