864ea84dddb59d62d2bedecc8f231ae04618d161
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-10-02 11:14:52 -0400

    modified mlu if statemetns

--- flowsa/USDA_ERS_MLU.py
+++ flowsa/USDA_ERS_MLU.py
@@ -32,22 +32,22 @@ def mlu_parse(dataframe_list, args):
 
     fips = get_all_state_FIPS_2()
     for index, row in df.iterrows():
-        if(int(row["Year"]) == int(args['year'])):
-            if(row["Region or State"] != "Northeast" and row["Region or State"] != "Lake States" and
-                    row["Region or State"] != "Corn Belt" and row["Region or State"] != "Northern Plains" and
-                    row["Region or State"] != "Appalachian" and row["Region or State"] != "Southeast" and
-                    row["Region or State"] != "Delta States" and row["Region or State"] != "Southern Plains" and
-                    row["Region or State"] != "Mountain" and row["Region or State"] != "Pacific" and
-                    row["Region or State"] != "48 States"):
-                if(row['Region or State'] == "U.S. total"):
+        if int(row["Year"]) == int(args['year']):
+            if (row["Region or State"] != "Northeast") & (row["Region or State"] != "Lake States") & \
+                    (row["Region or State"] != "Corn Belt") & (row["Region or State"] != "Northern Plains") & \
+                    (row["Region or State"] != "Appalachian") & (row["Region or State"] != "Southeast") & \
+                    (row["Region or State"] != "Delta States") & (row["Region or State"] != "Southern Plains") & \
+                    (row["Region or State"] != "Mountain") & (row["Region or State"] != "Pacific") & \
+                    (row["Region or State"] != "48 States"):
+                if row['Region or State'] == "U.S. total":
                     location = "00000"
                 else:
                     for i, fips_row in fips.iterrows():
-                        if(fips_row["State"] == row['Region or State']):
+                        if fips_row["State"] == row['Region or State']:
                             location = fips_row["FIPS_2"] + "000"
 
                 for col in df_columns:
-                    if(col != "SortOrder" and col != "Region" and col != "Region or State" and col != "Year"):
+                    if (col != "SortOrder") & (col != "Region") & (col != "Region or State") & (col != "Year"):
                         data["Class"] = "Land"
                         data["SourceName"] = "USDA_ERS_MLU"
                         # flownames are the same as ActivityConsumedBy for purposes of mapping elementary flows
