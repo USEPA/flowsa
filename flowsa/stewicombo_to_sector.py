828f74847338c7387ab91040a964e1d3a8d4965f
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-10-14 15:24:35 -0400

    add expand NAICS function to stewicombo

--- flowsa/stewicombo_to_sector.py
+++ flowsa/stewicombo_to_sector.py
@@ -10,11 +10,10 @@ data_format FBS_outside_flowsa with the function specified in FBS_datapull_fxn
 """
 
 import pandas as pd
-from flowsa.flowbyfunctions import assign_fips_location_system, add_missing_flow_by_fields,\
-    sector_disaggregation
+from flowsa.flowbyfunctions import assign_fips_location_system, add_missing_flow_by_fields
 from flowsa.mapping import map_elementary_flows
 from flowsa.common import flow_by_sector_fields, apply_county_FIPS, sector_level_key, \
-    update_geoscale, log
+    update_geoscale, log, load_sector_length_crosswalk_w_nonnaics
 
 def stewicombo_to_sector(inventory_dict, NAICS_level, geo_scale, compartments):
     """
@@ -28,7 +27,7 @@ def stewicombo_to_sector(inventory_dict, NAICS_level, geo_scale, compartments):
     :param compartments: list of compartments to include (e.g., 'water', 'air',
                 'soil'), use None to include all compartments
     """
-    
+
     from stewi.globals import output_dir as stw_output_dir
     from stewi.globals import weighted_average
     import stewi
@@ -70,6 +69,7 @@ def stewicombo_to_sector(inventory_dict, NAICS_level, geo_scale, compartments):
         inventories_of_interest_list=inventory_list)
     all_NAICS = all_NAICS.loc[all_NAICS['PRIMARY_INDICATOR']=='PRIMARY']
     all_NAICS.drop(columns=['PRIMARY_INDICATOR'], inplace=True)
+    all_NAICS = naics_expansion(all_NAICS)
     if len(all_NAICS[all_NAICS.duplicated(subset = ['FRS_ID','Source'], keep = False)])>0:
         log.info('Duplicate primary NAICS reported - keeping first')
         all_NAICS.drop_duplicates(subset = ['FRS_ID','Source'], 
@@ -145,11 +145,9 @@ def stewicombo_to_sector(inventory_dict, NAICS_level, geo_scale, compartments):
     fbs['SectorConsumedBy'] = 'None'
     fbs['SectorSourceName'] = 'NAICS_2012_Code'
     fbs['FlowType'] = 'ELEMENTARY_FLOW'
-
-    # carry out to 6-digit NAICS where possible
-    #fbs = sector_disaggregation(fbs)
     
     fbs = assign_fips_location_system(fbs, list(inventory_dict.values())[0])
+
     # add missing flow by sector fields
     fbs = add_missing_flow_by_fields(fbs, flow_by_sector_fields)
     
@@ -157,3 +155,56 @@ def stewicombo_to_sector(inventory_dict, NAICS_level, geo_scale, compartments):
     fbs = fbs.sort_values(list(flow_by_sector_fields.keys())).reset_index(drop=True)
 
     return fbs
+
+def naics_expansion(facility_NAICS):
+    """ modeled after sector_disaggregation in flowbyfunctions, updates NAICS 
+    to more granular sectors if there is only one naics at a lower level
+    :param facility_NAICS: df of facilities from facility matcher with NAICS
+    """
+
+    # load naics 2 to naics 6 crosswalk
+    cw_load = load_sector_length_crosswalk_w_nonnaics()
+    cw = cw_load[['NAICS_4', 'NAICS_5', 'NAICS_6']]
+
+    # subset the naics 4 and 5 columns
+    cw4 = cw_load[['NAICS_4', 'NAICS_5']]
+    cw4 = cw4.drop_duplicates(subset=['NAICS_4'], keep=False).reset_index(drop=True)
+    naics4 = cw4['NAICS_4'].values.tolist()
+
+    # subset the naics 5 and 6 columns
+    cw5 = cw_load[['NAICS_5', 'NAICS_6']]
+    cw5 = cw5.drop_duplicates(subset=['NAICS_5'], keep=False).reset_index(drop=True)
+    naics5 = cw5['NAICS_5'].values.tolist()
+
+    # for loop in reverse order longest length naics minus 1 to 2
+    # appends missing naics levels to df
+    for i in range(4, 6):
+        if i == 4:
+            sector_list = naics4
+            sector_merge = "NAICS_4"
+            sector_add = "NAICS_5"
+        elif i == 5:
+            sector_list = naics5
+            sector_merge = "NAICS_5"
+            sector_add = "NAICS_6"
+
+        # subset df to NAICS with length = i 
+        df_subset = facility_NAICS.loc[facility_NAICS["NAICS"].apply(lambda x: len(x) == i)]
+
+        # subset the df to the rows where the tmp sector columns are in naics list
+        df_subset = df_subset.loc[(df_subset['NAICS'].isin(sector_list))]
+
+        # merge the naics cw
+        new_naics = pd.merge(df_subset, cw[[sector_merge, sector_add]],
+                             how='left', left_on=['NAICS'], right_on=[sector_merge])
+        # drop columns and rename new sector columns
+        new_naics['NAICS'] = new_naics[sector_add]
+        new_naics = new_naics.drop(columns=[sector_merge, sector_add])
+
+        # drop records with NAICS that have now been expanded
+        facility_NAICS = facility_NAICS[~facility_NAICS['NAICS'].isin(sector_list)]
+
+        # append new naics to df
+        facility_NAICS = pd.concat([facility_NAICS, new_naics], sort=True)
+
+    return facility_NAICS
