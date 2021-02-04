cb0ca5267afc3fe5cf07267ef5e1b8533691722d
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2021-02-04 16:15:01 -0500

    update method assigning location system in usgs_wu_coef

--- scripts/write_FBA_USGS_WU_Coef.py
+++ scripts/write_FBA_USGS_WU_Coef.py
@@ -12,7 +12,7 @@ Data output saved as csv, retaining assigned file name "USGS_WU_Coef_Raw.csv"
 from flowsa.common import *
 import pandas as pd
 from flowsa.flowbyactivity import store_flowbyactivity
-from flowsa.flowbyfunctions import fba_fill_na_dict, clean_df
+from flowsa.flowbyfunctions import fba_fill_na_dict, clean_df, assign_fips_location_system
 
 
 # 2012--2018 fisheries data at state level
@@ -38,8 +38,8 @@ if __name__ == '__main__':
     df["Class"] = "Water"
     df["SourceName"] = "USGS_WU_Coef"
     df["Location"] = US_FIPS
-    df['LocationSystem'] = "FIPS_2015"  # state FIPS codes have not changed over last decade
     df['Year'] = 2005
+    df = assign_fips_location_system(df, '2005')
     df["Unit"] = "gallons/animal/day"
 
     # add missing dataframe fields (also converts columns to desired datatype)
