3065ae4946b62c570d783d6327e8f3a583d47b58
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2021-03-03 08:33:51 -0500

    change numpy requirements to < 1.20

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'xlrd>=1.2.0',
         'requests_ftp==0.3.1',
         'tabula-py>=2.1.1',
-        'numpy==1.19.1'
+        'numpy<=1.20'
     ],
     url='https://github.com/USEPA/FLOWSA',
     license='CC0',
