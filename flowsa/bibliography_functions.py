750dca9e8c7f3b798f39854d13d1bb265e2acfd7
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2021-03-11 11:35:38 -0500

    update bib fxn to include allocation sources

--- flowsa/bibliography_functions.py
+++ flowsa/bibliography_functions.py
@@ -2,8 +2,10 @@ import os
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 from flowsa.flowbysector import load_method
-from flowsa.common import outputpath, biboutputpath
+from flowsa.common import outputpath, biboutputpath, load_sourceconfig
 
+# test
+# methodname = 'Land_national_2012'
 
 def generate_fbs_bibliography(methodname):
     """
@@ -16,21 +18,46 @@ def generate_fbs_bibliography(methodname):
     fbs_yaml = load_method(methodname)
 
     # create dictionary of data and allocation datasets
-    fb = fbs_yaml['source_names']
+    fbs = fbs_yaml['source_names']
     bib_list = []
-    for k, v in fb.items():
+    for fbs_k, fbs_v in fbs.items():
+
+        # load the fba yaml
+        fba = load_sourceconfig(fbs_k)
+
         db = BibDatabase()
         db.entries = [{
-             'title': k,
-             'author': 'test',
-             'year': str(v['year']),
-             'url': 'test',
-             'urldate': 'test',
-             'ID': k + '_' + str(v['year']),
-             'ENTRYTYPE': 'misc'
+            'title': fba['source_name'],
+            'author': fba['author'],
+            'year': str(fbs_v['year']),
+            'url': fba['citeable_url'],
+            'urldate': fba['date_of_access'],
+            'ID': fbs_k + '_' + str(fbs_v['year']),
+            'ENTRYTYPE': 'misc'
         }]
         # append each entry to a list of BibDatabase entries
         bib_list.append(db)
+
+        # add information for activity sets
+        activities = fbs_v['activity_sets']
+        for aset, attr in activities.items():
+            if attr['allocation_source'] != 'None':
+
+                # load the fba yaml
+                fba_alloc = load_sourceconfig(attr['allocation_source'])
+
+                db = BibDatabase()
+                db.entries = [{
+                     'title': fba_alloc['source_name'],
+                     'author': fba_alloc['author'],
+                     'year': str(attr['allocation_source_year']),
+                     'url': fba_alloc['citeable_url'],
+                     'urldate': fba_alloc['citeable_url'],
+                     'ID': attr['allocation_source'] + '_' + str(attr['allocation_source_year']),
+                     'ENTRYTYPE': 'misc'
+                }]
+                # append each entry to a list of BibDatabase entries
+                bib_list.append(db)
 
     # write out bibliography
     writer = BibTexWriter()
