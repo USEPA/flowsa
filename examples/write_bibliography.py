83d5f22538192444d82d7be28ca533c4a4e50027
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2021-03-12 13:56:26 -0500

    add example script to produce bibliography

---
+++ examples/write_bibliography.py
@@ -0,0 +1,12 @@
+# __init__.py (flowsa)
+# !/usr/bin/env python3
+# coding=utf-8
+
+"""
+Writes a .bib for sources in a FlowBySector method yaml
+"""
+
+import flowsa
+
+# write bib file to local directory
+flowsa.writeFlowBySectorBibliography('Land_national_2012')
