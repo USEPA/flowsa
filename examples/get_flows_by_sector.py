cd19cfc0814dfddd40fe1f42205d940563742826
Catherine Birney <birney.catherine@epa.gov>
2021-04-08 09:29:13 -0400

    modify fbs example call

--- examples/get_flows_by_sector.py
+++ examples/get_flows_by_sector.py
@@ -4,8 +4,8 @@
 
 """
 Retrieves stored data in the FlowBySector format
-    :param methodname: string, Name of an available method for the given class. Method files found in
-                       flowsa/data/flowbysectormethods
+    :param methodname: string, Name of an available method for the given class.
+    Method files found in flowsa/data/flowbysectormethods
     :return: dataframe in flow by sector format
 """
 
@@ -14,5 +14,6 @@ import flowsa
 # load a FBS from local directory
 fbs_water = flowsa.getFlowBySector('Water_national_2015_m1')
 
-# collapse the FBS - output has 'Sector' column instead of 'SectorProducedBy' and 'SectorConsumedBy' columns
-fbs_water_collapsed = flowsa.collapse_FlowBySector('Water_national_2015_m2')
+# collapse the FBS - output has 'Sector' column instead of
+# 'SectorProducedBy' and 'SectorConsumedBy' columns
+fbs_water_collapsed = flowsa.collapse_FlowBySector('Water_national_2015_m1')
