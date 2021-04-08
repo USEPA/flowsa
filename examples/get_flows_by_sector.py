10bf16ffd98feb1b9e5ddad2b7d1282fe77cf4db
Catherine Birney <birney.catherine@epa.gov>
2021-04-08 13:40:16 -0400

    modify comments in example

--- examples/get_flows_by_sector.py
+++ examples/get_flows_by_sector.py
@@ -11,7 +11,7 @@ Retrieves stored data in the FlowBySector format
 
 import flowsa
 
-# load a FBS from local directory
+# load FBS from local directory, if does not exist, method will run
 fbs_water = flowsa.getFlowBySector('Water_national_2015_m1')
 
 # collapse the FBS - output has 'Sector' column instead of
