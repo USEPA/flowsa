8a542ad7d0579ace56c5b666c47888c5893055cb
Catherine Birney <birney.catherine@epa.gov>
2021-04-28 10:25:17 -0400

    merge crosswalk scripts for eia cbecs land and eia cbecs water into one .py

--- scripts/FlowByActivity_Crosswalks/write_Crosswalk_EIA_CBECS.py
+++ scripts/FlowByActivity_Crosswalks/write_Crosswalk_EIA_CBECS.py
@@ -1,17 +1,21 @@
-# write_Crosswalk_EIA_CBECS_Land.py (scripts)
+# write_Crosswalk_EIA_CBECS.py (scripts)
 # !/usr/bin/env python3
 # coding=utf-8
 
 """
 Download the crosswalk from CBECS to NAICS that CBECS publishes and reshape
 
+Script creates crosswalks for Land and Water
+
 """
-import pandas as pd
+
 import io
+import pandas as pd
 from flowsa.common import datapath, make_http_request
 from flowsa.data_source_scripts.EIA_CBECS_Land import standardize_eia_cbecs_land_activity_names
 
 if __name__ == '__main__':
+
     # url for excel crosswalk
     url = 'http://www.eia.gov/consumption/commercial/data/archive/cbecs/PBAvsNAICS.xls'
     # make url requestl, as defined in common.py
@@ -42,20 +46,26 @@ if __name__ == '__main__':
         df = df.append(pd.DataFrame([[s, 'Vacant']],
                                     columns=['Sector', 'Activity']), ignore_index=True, sort=False)
 
-    # Add additional columns
-    df['ActivitySourceName'] = "EIA_CBECS_Land"
     # the original dataset is for NAICS 2002, but 3 digit NAICS have not changed between 2002 and 2012, so labeling 2012
     df['SectorSourceName'] = "NAICS_2012_Code"
     df['SectorType'] = "I"
 
-    # standarize activity names to match those in FBA
-    df = standardize_eia_cbecs_land_activity_names(df, 'Activity')
-    # reorder and drop columns
-    df = df[['ActivitySourceName', 'Activity', 'SectorSourceName', 'Sector', 'SectorType']]
+    # create crosswalks for water and land
+    resource = ['Water', 'Land']
+    for r in resource:
+        # Add additional columns
+        df['ActivitySourceName'] = "EIA_CBECS_" + r
 
-    # sort df
-    df = df.sort_values(['Activity', 'Sector']).reset_index(drop=True)
+        if r == 'Land':
+            # standarize activity names to match those in FBA
+            df = standardize_eia_cbecs_land_activity_names(df, 'Activity')
 
-    # save as csv
-    df.to_csv(datapath + "activitytosectormapping/" + "Crosswalk_EIA_CBECS_Land_toNAICS.csv", index=False)
+        # reorder and drop columns
+        df = df[['ActivitySourceName', 'Activity', 'SectorSourceName', 'Sector', 'SectorType']]
+
+        # sort df
+        df = df.sort_values(['Activity', 'Sector']).reset_index(drop=True)
+
+        # save as csv
+        df.to_csv(datapath + "activitytosectormapping/" + "Crosswalk_EIA_CBECS_" + r + "_toNAICS.csv", index=False)
 
