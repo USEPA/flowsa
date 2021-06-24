5e0254ca71d8deef40736a5c5743af6d8cd97574
Catherine Birney <birney.catherine@epa.gov>
2021-06-24 14:27:16 -0400

    initial parsing for acup npag/pesticide data

--- flowsa/data_source_scripts/USDA_ACUP.py
+++ flowsa/data_source_scripts/USDA_ACUP.py
@@ -76,16 +76,57 @@ def acup_parse(**kwargs):
                    args: dictionary, used to run flowbyactivity.py ('year' and 'source')
     :return: df, parsed and partially formatted to flowbyactivity specifications
     """
+
     # load arguments necessary for function
     dataframe_list = kwargs['dataframe_list']
     args = kwargs['args']
 
-    df = pd.concat(dataframe_list, sort=False)
+    df = pd.concat(dataframe_list, ignore_index=True)
+
+    # drop unused columns
+    df = df.drop(columns=['agg_level_desc', 'location_desc', 'state_alpha', 'sector_desc',
+                          'country_code', 'begin_code', 'watershed_code', 'reference_period_desc',
+                          'asd_desc', 'county_name', 'source_desc', 'congr_district_code', 'asd_code',
+                          'week_ending', 'freq_desc', 'load_time', 'zip_5', 'watershed_desc', 'region_desc',
+                          'state_ansi', 'state_name', 'country_name', 'county_ansi', 'end_code', 'group_desc'])
+    # create FIPS column by combining existing columns
+    df.loc[df['county_code'] == '', 'county_code'] = '000'  # add county fips when missing
+    df['Location'] = df['state_fips_code'] + df['county_code']
+    df.loc[df['Location'] == '99000', 'Location'] = US_FIPS  # modify national level fips
+
+    # address non-NAICS classification data
+    # use info from other columns to determine flow name
+    df.loc[:, 'FlowName'] = df['statisticcat_desc'] + ', ' + df['prodn_practice_desc']
+    # combine column information to create activity information, and create two new columns for activities
+    df['ActivityConsumedBy'] = df['commodity_desc'] + ', ' + df['class_desc']
+
+    # rename columns to match flowbyactivity format
+    df = df.rename(columns={"Value": "FlowAmount", "unit_desc": "Unit",
+                            "year": "Year", "CV (%)": "Spread",
+                            "short_desc": "Description"})
+    # drop remaining unused columns
+    df = df.drop(columns=['class_desc', 'commodity_desc', 'domain_desc', 'state_fips_code', 'county_code',
+                          'statisticcat_desc', 'prodn_practice_desc', 'domaincat_desc', 'util_practice_desc'])
+    # modify contents of flowamount column, "D" is supressed data, "z" means less than half the unit is shown
+    df['FlowAmount'] = df['FlowAmount'].str.strip()  # trim whitespace
+    df.loc[df['FlowAmount'] == "(D)", 'FlowAmount'] = withdrawn_keyword
+    df.loc[df['FlowAmount'] == "(Z)", 'FlowAmount'] = withdrawn_keyword
+    df['FlowAmount'] = df['FlowAmount'].str.replace(",", "", regex=True)
+    # USDA CoA 2017 states that (H) means CV >= 99.95, therefore replacing with 99.95 so can convert column to int
+    # (L) is a CV of <= 0.05
+    df['Spread'] = df['Spread'].str.strip()  # trim whitespace
+    df.loc[df['Spread'] == "(H)", 'Spread'] = 99.95
+    df.loc[df['Spread'] == "(L)", 'Spread'] = 0.05
+    df.loc[df['Spread'] == "", 'Spread'] = None  # for instances where data is missing
+    df.loc[df['Spread'] == "(D)", 'Spread'] = withdrawn_keyword
 
     # add location system based on year of data
     df = assign_fips_location_system(df, args['year'])
     # Add hardcoded data
-    df['SourceName'] = "USDA_ACUP_Pestice"
+    df['Class'] = "Chemicals"
+    df['SourceName'] = args['source']
+    df['FlowType'] = 'ELEMENTARY_FLOW'
+    df['MeasureofSpread'] = "RSD"
     df['DataReliability'] = 5  # tmp
     df['DataCollection'] = 5  # tmp
 
