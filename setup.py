041579d7bd24608f8773118d2c4aff986913a3a8
Catherine Birney <birney.catherine@epa.gov>
2021-06-30 07:47:18 -0400

    typo

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'fedelemflowlist @ git+https://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List@v1.0.7#egg=fedelemflowlist',
-        'esupy @ git+https://github.com/USEPA/esupy@v0.1.1@v0.1.1#egg=esupy',
-        'StEWI @ git+https://github.com/USEPA/standardizedinventories@v0.9.6#egg=StEWI@v0.9.6#egg=StEWI',
+        'esupy @ git+https://github.com/USEPA/esupy@v0.1.1#egg=esupy',
+        'StEWI @ git+https://github.com/USEPA/standardizedinventories@v0.9.6#egg=StEWI',
         'pandas>=1.1.0',
         'pip>=9',
         'setuptools>=41',
