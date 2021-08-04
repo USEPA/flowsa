d2d36181224b18c11ef34e6b86350c622d3f7090
Andrew Beck <andrew.beck@erg.com>
2021-08-04 14:13:46 -0400

    Created FBA method file and data source script (call+parse) for EPA's CDDPath v2 data

---
+++ flowsa/data_source_scripts/EPA_CDDPath.py
@@ -0,0 +1,93 @@
+# EPA_CDDPath.py (scripts)
+# !/usr/bin/env python3
+# coding=utf-8
+"""
+Construction and Demolition Debris 2014 Final Disposition Estimates Using the CDDPath Method v2
+https://edg.epa.gov/metadata/catalog/search/resource/details.page?uuid=https://doi.org/10.23719/1503167
+Last updated: 2018-11-07
+"""
+
+import io
+import pandas as pd
+#import numpy as np
+#import flowsa
+# from flowsa.common import US_FIPS, flow_by_activity_fields, fba_fill_na_dict
+# from flowsa.flowbyfunctions import assign_fips_location_system, \
+#     proportional_allocation_by_location_and_activity, filter_by_geoscale
+# from flowsa.dataclean import harmonize_units, clean_df
+# from flowsa.mapping import add_sectors_to_flowbyactivity
+# from flowsa.data_source_scripts.BLS_QCEW import clean_bls_qcew_fba
+
+# =============================================================================
+# ## Import statements from EIA_CBECS_Land.py: ##
+# from flowsa.common import US_FIPS, get_region_and_division_codes, withdrawn_keyword,\
+#     clean_str_and_capitalize, fba_default_grouping_fields
+# from flowsa.flowbyfunctions import assign_fips_location_system, aggregator
+# from flowsa.values_from_literature import \
+#     get_commercial_and_manufacturing_floorspace_to_land_area_ratio
+# =============================================================================
+
+
+# Read pdf into list of DataFrame
+def epa_cddpath_call(**kwargs):
+    """
+    Convert response for calling url to pandas dataframe, begin parsing df into FBA format
+    :param kwargs: potential arguments include:
+                   url: string, url
+                   response_load: df, response from url call
+                   args: dictionary, arguments specified when running
+                   flowbyactivity.py ('year' and 'source')
+    :return: pandas dataframe of original source data
+    """
+    # load arguments necessary for function
+    response_load = kwargs['r']
+
+    # Convert response to dataframe
+    df = (pd.io.excel.read_excel(io.BytesIO(response_load.content),
+                                 sheet_name='Final Results',
+                                 # exclude extraneous rows & cols
+                                 header=2, nrows=30, usecols="A, B, E",
+                                 # give columns tidy names
+                                 names=["FlowName", "landfilled", "processed"],
+                                 # specify data types
+                                 dtype={'a': str, 'b': float, 'e': float})
+          .dropna()  # drop NaN's produced by Excel cell merges
+          .melt(id_vars=["FlowName"],
+                var_name="Description",
+                value_name="FlowAmount"))
+
+    return df
+
+
+def epa_cddpath_parse(**kwargs):
+    """
+    Combine, parse, and format the provided dataframes
+    :param kwargs: potential arguments include:
+                   dataframe_list: list of dataframes to concat and format
+                   args: dictionary, used to run flowbyactivity.py ('year' and 'source')
+    :return: df, parsed and partially formatted to flowbyactivity specifications
+    """
+    # load arguments necessary for function
+    dataframe_list = kwargs['dataframe_list']
+
+    # concat list of dataframes (info on each page)
+    df = pd.concat(dataframe_list, sort=False)
+    
+    # hardcode
+    # [AB] commented out lines I wasn't relatively sure of
+    df['Class'] = 'Land'  # confirm this
+    df['SourceName'] = 'EPA_CDDPath'  # confirm this
+    df['FlowName'] = 'CDD Disposition Estimates, US 2014'  # confirm this
+    df['Unit'] = 'gal/USD'
+    df['FlowType'] = 'WASTE_FLOW'
+    # df['ActivityProducedBy'] = "Construction and Demolition"  # confirm this
+    # df['ActivityConsumedBy'] = "Waste Management"  # confirm this
+    # df['Compartment'] = 'ground'  # confirm this
+    # df['Location'] = US_FIPS
+    # df = assign_fips_location_system(df, '2002')
+    df['Year'] = '2014'
+    # df['MeasureofSpread'] = "NA"  # none available
+    df['DataReliability'] = 5  # tmp
+    df['DataCollection'] = 5  #tmp
+
+    return df
