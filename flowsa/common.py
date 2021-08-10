47c29c10763af281beb8c6ca886b758fc07e5e5c
Catherine Birney <birney.catherine@epa.gov>
2021-08-10 16:18:03 -0400

    add flowuuid as df requirement in dictionary and specs

--- flowsa/common.py
+++ flowsa/common.py
@@ -294,7 +294,8 @@ flow_by_sector_fields = \
      'GeographicalCorrelation': [{'dtype': 'float'}, {'required': True}],
      'TechnologicalCorrelation': [{'dtype': 'float'}, {'required': True}],
      'DataCollection': [{'dtype': 'float'}, {'required': True}],
-     'MetaSources': [{'dtype': 'str'}, {'required': True}]
+     'MetaSources': [{'dtype': 'str'}, {'required': True}],
+     'FlowUUID': [{'dtype': 'str'}, {'required': True}]
      }
 
 flow_by_sector_fields_w_activity = flow_by_sector_fields.copy()
@@ -325,7 +326,8 @@ flow_by_sector_collapsed_fields = \
      'GeographicalCorrelation': [{'dtype': 'float'}, {'required': True}],
      'TechnologicalCorrelation': [{'dtype': 'float'}, {'required': True}],
      'DataCollection': [{'dtype': 'float'}, {'required': True}],
-     'MetaSources': [{'dtype': 'str'}, {'required': True}]
+     'MetaSources': [{'dtype': 'str'}, {'required': True}],
+     'FlowUUID': [{'dtype': 'str'}, {'required': True}]
      }
 
 flow_by_activity_mapped_fields = \
