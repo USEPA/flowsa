73a7564e808d2fa48eff7433588a48ac40617f79
Catherine Birney <birney.catherine@epa.gov>
2021-08-12 14:58:59 -0400

    update state FBS employment method to estimate suppressed data using equal allocation

--- flowsa/data_source_scripts/BLS_QCEW.py
+++ flowsa/data_source_scripts/BLS_QCEW.py
@@ -17,7 +17,7 @@ import pandas as pd
 import numpy as np
 from flowsa.common import US_FIPS, fba_default_grouping_fields, flow_by_activity_wsec_mapped_fields
 from flowsa.flowbyfunctions import assign_fips_location_system, \
-    aggregator
+    aggregator, estimate_suppressed_data, log
 from flowsa.dataclean import add_missing_flow_by_fields, replace_strings_with_NoneType
 
 
@@ -259,3 +259,22 @@ def bls_clean_allocation_fba_w_sec(df_w_sec, **kwargs):
     df3 = replace_strings_with_NoneType(df2)
 
     return df3
+
+
+def bls_clean_allocation_fba_w_sec_state(df_w_sec, **kwargs):
+    """
+   clean up bls df with sectors by estimating suppressed data
+   :param df_w_sec: df, FBA format BLS QCEW data
+   :param kwargs: additional arguments can include 'attr', a
+   dictionary of FBA method yaml parameters
+   :return: df, BLS QCEW FBA with estimated suppressed data
+   """
+    # first adjust using national level methods
+    df = bls_clean_allocation_fba_w_sec(df_w_sec, **kwargs)
+
+    # estimate the suppressed data by equally allocating parent naics to child
+    log.info('Estimating suppressed data by equally allocating '
+             'parent NAICS to child NAICS')
+    df = estimate_suppressed_data(df, 'SectorProducedBy', 3, kwargs['sourcename'])
+
+    return df
