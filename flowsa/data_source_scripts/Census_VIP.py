f2ce3621a0d902aa3a152a2011a09e59569a3d81
Ben Young <Ben.Young@erg.com>
2021-08-15 16:43:26 -0400

    parse Census VIP data

--- flowsa/data_source_scripts/Census_VIP.py
+++ flowsa/data_source_scripts/Census_VIP.py
@@ -8,7 +8,8 @@ https://www.census.gov/construction/c30/c30index.html
 """
 import pandas as pd
 import numpy as np
-
+from flowsa.flowbyfunctions import assign_fips_location_system
+from flowsa.common import US_FIPS
 
 def census_vip_call(**kwargs):
     """
@@ -56,9 +57,24 @@ def census_vip_parse(**kwargs):
     args = kwargs['args']
 
     df = pd.concat(dataframe_list, sort=False)
-    
-    # parse data
+    df['Year'] = df['Year'].astype(str)
+    df = df[df['Year'] == args['year']].reset_index(drop=True)
+    df = df.rename(columns = {'Type of Construction:':'ActivityProducedBy'})
+
+    df['Class'] = 'Money'
+    df['SourceName'] = 'Census_VIP'
+    df['FlowName'] = 'Construction spending'
     # millions of dollars
+    df['FlowAmount'] = df['FlowAmount'] * 1000000
+    df['Unit'] = 'USD'
+    df['FlowType'] = "ELEMENTARY_FLOW"
+    df['Compartment'] = None
+    df['Location'] = US_FIPS
+    df = assign_fips_location_system(df, args['year'])
+    df['Year'] = args['year']
+    # Add tmp DQ scores
+    df['DataReliability'] = 5
+    df['DataCollection'] = 5
     
     return df
 
