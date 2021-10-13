738cc665e38371dc361d7aee0dd7d3e9549224c6
Catherine Birney <birney.catherine@epa.gov>
2021-10-13 14:36:36 -0400

    move examples back outside flowsa directory

--- flowsa/test_examples.py
+++ flowsa/test_examples.py
@@ -1,4 +1,4 @@
-from flowsa.examples import get_flows_by_activity, get_flows_by_sector, write_bibliography
+from examples import get_flows_by_sector, write_bibliography, get_flows_by_activity
 
 
 def test_write_bibliography():
