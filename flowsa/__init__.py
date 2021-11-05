f329ad8527cba750594858e042bfa19f6787aa1e
catherinebirney <birney.catherine@epa.gov>
2021-11-05 13:29:35 -0400

    clarify how files are loaded in the init file

--- flowsa/__init__.py
+++ flowsa/__init__.py
@@ -4,6 +4,15 @@
 """
 Public API for flowsa
 For standard dataframe formats, see https://github.com/USEPA/flowsa/tree/master/format%20specs
+
+Files are loaded from a user's local directory
+https://github.com/USEPA/flowsa/wiki/Data-Storage#local-storage
+
+or can be downloaded from a remote repository
+https://edap-ord-data-commons.s3.amazonaws.com/index.html?prefix=flowsa/
+
+The most recent version (based on timestamp) of Flow-By-Activity and
+Flow-By-Sector files are loaded when running these functions
 """
 import os
 import pprint
