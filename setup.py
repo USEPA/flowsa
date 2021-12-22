6fbda29bbe24d6d9fe5fb5828a93b529c4080ee8
catherinebirney <birney.catherine@epa.gov>
2021-12-22 15:11:15 -0500

    update flowsa version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flowsa',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     package_dir={'flowsa': 'flowsa'},
     package_data={'flowsa': ["data/*.*"]},
