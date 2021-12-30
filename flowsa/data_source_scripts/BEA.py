ffd1377955ced889d18404e117147a0c17469ac3
Ben Young <Ben.Young@erg.com>
2021-12-30 10:06:29 -0500

    drop rows with no sectors

--- flowsa/data_source_scripts/BEA.py
+++ flowsa/data_source_scripts/BEA.py
@@ -199,4 +199,6 @@ def subset_and_allocate_BEA_table(df, attr, **_):
              "clean_helper_fba": "clean_bls_qcew_fba",
              "clean_helper_fba_wsec": "bls_clean_allocation_fba_w_sec"}
     df2 = allocation_helper(df, attr2, method2, v, False)
+    # Drop remaining rows with no sectors e.g. T001 and other final demands
+    df2 = df2.dropna(subset=['SectorConsumedBy'])
     return df2
