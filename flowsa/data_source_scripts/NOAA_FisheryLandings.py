e0fe83f2bd77fd9b984ddf2dfa8dc9861223caf6
Ben Young <Ben.Young@erg.com>
2022-01-13 13:45:27 -0500

    fix additional import statement

--- flowsa/data_source_scripts/NOAA_FisheryLandings.py
+++ flowsa/data_source_scripts/NOAA_FisheryLandings.py
@@ -22,7 +22,7 @@ Data output saved as csv, retaining assigned file name "foss_landings.csv"
 
 import pandas as pd
 from flowsa.flowbyfunctions import assign_fips_location_system
-from flowsa.common import get_state_FIPS
+from flowsa.location import get_state_FIPS
 from flowsa.settings import externaldatapath
 
 
