e2409b0bbd0ef4f4c169f4e1f7e9ca226f9bbe93
matthewlchambers <matthew.chambers@bea.gov>
2022-01-27 12:35:59 -0500

    Finished? calling and parsing fxns for BTS_TSA

--- flowsa/data_source_scripts/BTS_TSA.py
+++ flowsa/data_source_scripts/BTS_TSA.py
@@ -21,9 +21,51 @@ def tsa_call(*, resp, year, **_):
     :return: pandas dataframe of original source data
     '''
     df = pd.read_csv(BytesIO(resp.content))
-    return df[df.year == year]
+    df = df[df.year == year]
+    return df
 
 
 def tsa_parse(*, df_list, source, year, config, **_):
-    df = (pd.concat(df_list, sort=True)
-          .rename(columns=config['parse']['rename_columns']))
+    df = pd.concat(df_list, sort=True)
+
+    # Data on in-house production of transportation services for own use
+    # comes from the Use table portion of the TSA
+    use = (df[df.table_name.str.match('Use')
+              & df.level_of_detail.str.match('Summary')]
+           .drop(columns=df.columns.difference(
+               list(config['parse']['rename_columns_use'].keys())
+           ))
+           .rename(columns=config['parse']['rename_columns_use']))
+    in_house = use[use.Description.str.startswith('In-house')]
+
+    # Data on for-hire production of transportation services (which may
+    # be done as an ancillary or secondary activity), comes from the Make
+    # table portion of the TSA
+    make = (df[df.table_name.str.match('Make')
+               & df.level_of_detail.str.match('Summary')]
+            .drop(columns=df.columns.difference(
+               list(config['parse']['rename_columns_make'].keys())
+            ))
+            .rename(columns=config['parse']['rename_columns_make']))
+    for_hire = make[make.Description.str.startswith('For-hire')]
+
+    df = pd.concat([in_house, for_hire])
+
+    # Original units are in million USD
+    df['FlowAmount'] = df['FlowAmount'] * 1000000
+
+    # Add other columns as needed for complete FBA
+    df['Class'] = 'Money'
+    df['SourceName'] = source
+    df['FlowName'] = 'Gross Output'
+    df['Unit'] = 'USD'
+    df['FlowType'] = 'TECHNOSPHERE_FLOW'
+    # df['ActivityConsumedBy'] = ''
+    # df['Compartment'] = ''  # ???
+    # df['Location'] = ''
+    # df['LocationSystem'] = ''
+    # df['MeasureofSpread'] = ''
+    df['DataReliability'] = 3  # temp
+    df['DataCollection'] = 5  # temp
+
+    return df
