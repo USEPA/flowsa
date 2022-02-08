17fcfaf3a8056afbeb431fcc782593dd52c56b94
melissagqc <melissa@gqc.com>
2022-02-08 16:21:16 -0500

    EPA_WFR ready for review

--- flowsa/data_source_scripts/EPA_WFR.py
+++ flowsa/data_source_scripts/EPA_WFR.py
@@ -11,6 +11,7 @@ import pandas as pd
 # import numpy as np
 from flowsa.common import US_FIPS
 from flowsa.flowbyfunctions import assign_fips_location_system
+from string import ascii_letters, ascii_uppercase
 
 
 def epa_wfr_call(*, resp, **_):
@@ -23,6 +24,7 @@ def epa_wfr_call(*, resp, **_):
         flowbyactivity.py ('year' and 'source')
     :return: pandas dataframe of original source data
     """
+    result_list = []
     df_list = []
     result = pd.DataFrame()
     df = pd.DataFrame()
@@ -33,23 +35,22 @@ def epa_wfr_call(*, resp, **_):
         if len(df_l[0].columns) == 12:
             df = df_l[0].set_axis(
                 ['MANAGEMENT PATHWAY', 'MANUFACTURING/ PROCESSING', 'RESIDENTIAL', 'RETAIL', 'WHOLESALE', 'HOTELS',
-                 'seven', 'eight', 'K-12 SCHOOLS',  'FOOD BANKS', 'INTERMEDIATE AMOUNT MANAGED1',
-                 'TOTAL MANAGED BY EACH PATHWAY 2'], axis=1, inplace=False)
+                 'seven', 'eight', 'K-12 SCHOOLS',  'FOOD BANKS', 'INTERMEDIATE AMOUNT MANAGED',
+                 'TOTAL MANAGED BY EACH PATHWAY'], axis=1, inplace=False)
         else:
             df = df_l[0].set_axis(
                 ['MANAGEMENT PATHWAY', 'MANUFACTURING/ PROCESSING', 'RESIDENTIAL', 'RETAIL', 'WHOLESALE', 'HOTELS',
-                 'seven', 'K-12 SCHOOLS', 'FOOD BANKS', 'INTERMEDIATE AMOUNT MANAGED1',
-                 'TOTAL MANAGED BY EACH PATHWAY 2'], axis=1, inplace=False)
+                 'seven', 'K-12 SCHOOLS', 'FOOD BANKS', 'INTERMEDIATE AMOUNT MANAGED',
+                 'TOTAL MANAGED BY EACH PATHWAY'], axis=1, inplace=False)
         df = drop_rows(df)
         df_list.append(df)
     for d in df_list:
         result = result.append(d)
     result = fix_row_names(result)
     result = split_problem_column(result)
-
-
-
-    return result
+    result = reorder_df(result)
+    result_list.append(result)
+    return result_list
 
 
 def epa_wfr_parse(*, df_list, **_):
@@ -61,23 +62,19 @@ def epa_wfr_parse(*, df_list, **_):
     """
     # concat list of dataframes (info on each page)
     df = pd.concat(df_list, sort=False)
-    df = df.rename(columns={"I-O code": "ActivityConsumedBy",
-                            "I-O description": "Description",
-                            "gal/$M": "FlowAmount",
-                            })
-    # hardcode
-    # original data in gal/million usd
-    df.loc[:, 'FlowAmount'] = df['FlowAmount'] / 1000000
-    df['Unit'] = 'gal/USD'
-    df['SourceName'] = 'Blackhurst_IO'
-    df['Class'] = 'Water'
-    df['FlowName'] = 'Water Withdrawals IO Vector'
-    df['Location'] = US_FIPS
-    df = assign_fips_location_system(df, '2002')
-    df['Year'] = '2002'
-    df['DataReliability'] = 5  # tmp
-    df['DataCollection'] = 5  # tmp
 
+    # hardcode
+    # original data in short tons
+    df['Class'] = 'Other'
+    df['SourceName'] = 'EPA_WFR'
+    df['FlowName'] = 'Food Waste'
+    df['FlowType'] = 'WASTE_FLOW'
+    df['Compartment '] = 'None'
+    df['Location'] = US_FIPS
+    df = assign_fips_location_system(df, '2018')
+    df['Year'] = '2018'
+    df['Unit'] = 'short tons'
+    df['Description'] = 'EXCESS FOOD AND FOOD WASTE MANAGED BY SECTOR (TONS)'
     return df
 
 def drop_rows(df):
@@ -96,6 +93,8 @@ def fix_row_names(df):
     return df
 
 def split_problem_column(df):
+    t = str.maketrans('', '', ascii_uppercase)
+    seven_array_corrected = []
     restaurants_list = []
     sports_list = []
     hospitals_list = []
@@ -106,10 +105,23 @@ def split_problem_column(df):
     colleges_list = []
     index = 0
 
+
     col_seven_list = df['seven'].tolist()
     col_eight_list = df['eight'].tolist()
     for i in col_seven_list:
-        seven_array = i.split(" ")
+        strip = i.translate(t)
+        seven_array = strip.split(" ")
+        seven_array = ' '.join(seven_array).split()
+        if len(seven_array) == 8:
+            for idx, val in enumerate(seven_array):
+                seven_array[idx] = val
+                if val[0] == ",":
+                    value_str = seven_array[idx - 1]
+                    value_str = value_str + val
+                    seven_array[idx - 1] = value_str
+                    seven_array[idx] = ""
+            seven_array = ' '.join(seven_array).split()
+
         restaurants_list.append(seven_array[0])
         sports_list.append(seven_array[1])
         hospitals_list.append(seven_array[2])
@@ -117,6 +129,7 @@ def split_problem_column(df):
         military_list.append(seven_array[4])
         office_list.append(seven_array[5])
         correctional_list.append(seven_array[6])
+
         if len(seven_array) == 7:
             colleges_list.append(col_eight_list[index])
         else:
@@ -131,7 +144,16 @@ def split_problem_column(df):
     df['OFFICE BUILDINGS'] = office_list
     df['CORRECTIONAL FACILITIES'] = correctional_list
     df['COLLEGES & UNIVERSITIES'] = colleges_list
-    df = df.drop(columns=['seven', 'eight', 'INTERMEDIATE AMOUNT MANAGED1',
-                 'TOTAL MANAGED BY EACH PATHWAY 2'])
+    df = df.drop(columns=['seven', 'eight', 'INTERMEDIATE AMOUNT MANAGED',
+                 'TOTAL MANAGED BY EACH PATHWAY'])
+
     return df
 
+def reorder_df(df):
+    df = df.melt(id_vars="ActivityConsumedBy", var_name="ActivityProducedBy", value_name="FlowAmount")
+    indexResult = df[df['FlowAmount'] == '-'].index
+    df = df.replace(',', '', regex=True)
+    df.drop(indexResult, inplace=True)
+    df = df.reset_index()
+    df = df.drop(columns=['index'])
+    return df
