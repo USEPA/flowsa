9f4d4939e218bd55f5ed9849c133ce444265fc83
melissagqc <melissa@gqc.com>
2022-02-09 09:27:07 -0500

    Commented the new code for easier reading.

--- flowsa/data_source_scripts/EPA_WFR.py
+++ flowsa/data_source_scripts/EPA_WFR.py
@@ -8,7 +8,6 @@ Scrapes data from 2018 Wasted Food Report.
 import io
 import tabula
 import pandas as pd
-# import numpy as np
 from flowsa.common import US_FIPS
 from flowsa.flowbyfunctions import assign_fips_location_system
 from string import ascii_letters, ascii_uppercase
@@ -53,7 +52,7 @@ def epa_wfr_call(*, resp, **_):
     return result_list
 
 
-def epa_wfr_parse(*, df_list, **_):
+def epa_wfr_parse(*, df_list, year, **_):
     """
     Combine, parse, and format the provided dataframes
     :param df_list: list of dataframes to concat and format
@@ -71,18 +70,35 @@ def epa_wfr_parse(*, df_list, **_):
     df['FlowType'] = 'WASTE_FLOW'
     df['Compartment '] = 'None'
     df['Location'] = US_FIPS
-    df = assign_fips_location_system(df, '2018')
-    df['Year'] = '2018'
+    df = assign_fips_location_system(df, year)
+    df['Year'] = year
     df['Unit'] = 'short tons'
     df['Description'] = 'EXCESS FOOD AND FOOD WASTE MANAGED BY SECTOR (TONS)'
     return df
 
 def drop_rows(df):
+    """
+    This drops the column headers for the table which ended up being scraped as 4 rows.
+    We are also dropping all rows with null values under the MANUFACTURING/ PROCESSING
+    section. When the PDF was scraped additional rows were added due to how the MANUFACTURING/ PROCESSING
+    column was formatted. Since - was used instead of NaN in the PDF to indicate something did not exist
+    NaN values could be used to get rid of artificial rows as they were the only ones with NaN values in them.
+    :param df: dataframe
+    :return: df
+    """
     df = df.drop(index=[0, 1, 2, 3])
     df = df.dropna(axis=0, subset=['MANUFACTURING/ PROCESSING'])
     return df
 
 def fix_row_names(df):
+    """
+    The Scrape from the PDF caused some of the names to end up in more than 1 row. This also caused artificial
+    rows which have been dealt with in another method. The dataframe is in order so a new list is added to the
+    dataframe it is ActivityConsumedBy. We then reindex the dataframe and drop the last two columns. They are
+    total columns and are not wanted in the parquet file.
+    :param df: dataframe
+    :return: df
+    """
     acb = ['Food Donation', 'Animal Feed', 'Codigestion/ Anaerobic Digestion', 'Composting/ Aerobic Processes',
                    'Bio-based Materials/ Biochemical Processing', 'Land Application', 'Sewer/ Wastewater Treatment',
                    'Landfill', 'Controlled Combustion', 'Total Food Waste & Excess Food', 'Percent of Total']
@@ -93,6 +109,20 @@ def fix_row_names(df):
     return df
 
 def split_problem_column(df):
+    """
+     When the table was scraped from the PDF the 7th
+     and 8th columns were not correct. Depending on
+     the page the table was printed on the seventh
+     column either 7 or 8 data points in it.
+     This method takes the 7th and 8th column and
+     splits them into lists and then adds them to
+     the dataframe. Additionally the Scrape for the
+     PDF caused letters to be in some of the numbers
+     They are removed. Also at one point a number gets
+     split into 2 columns that is also taken care of here.
+    :param df: dataframe
+    :return: df
+     """
     t = str.maketrans('', '', ascii_uppercase)
     seven_array_corrected = []
     restaurants_list = []
@@ -150,6 +180,14 @@ def split_problem_column(df):
     return df
 
 def reorder_df(df):
+    """
+    Melts the pandas dataframe into flow by activity format.
+    Drops any rows where flow amount is -
+    Gets rid of the commas in the flow amount.
+    Resets the index of the dataframe and returns the dataframe
+    :param df: dataframe
+    :return: df
+    """
     df = df.melt(id_vars="ActivityConsumedBy", var_name="ActivityProducedBy", value_name="FlowAmount")
     indexResult = df[df['FlowAmount'] == '-'].index
     df = df.replace(',', '', regex=True)
