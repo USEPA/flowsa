a02cf3c696ba71e56106d7166207044eb3f88037
unknown <melissa@gqc.com>
2022-06-13 08:40:02 -0400

    EIA_AEO frist draft

--- flowsa/data_source_scripts/EIA_AEO.py
+++ flowsa/data_source_scripts/EIA_AEO.py
@@ -7,11 +7,13 @@ ANNUAL ENERGY OUTLOOK (AEO)
 https://www.eia.gov/outlooks/aeo/
 """
 
+import json
 import pandas as pd
 import numpy as np
 import math
 from flowsa.settings import externaldatapath
 from flowsa.common import load_api_key
+from flowsa.flowbyfunctions import assign_fips_location_system
 
 
 def eia_aeo_url_helper(*, build_url, year, config, **_):
@@ -63,8 +65,91 @@ def eia_aeo_url_helper(*, build_url, year, config, **_):
         print(userAPIKey)
         url = url.replace("__API_KEY__", userAPIKey)
         url = url.replace("__SERIES_ID__", series_list)
+        print(url)
         urls.append(url)
     
-    print(urls)
+  #  print(urls)
 
     return urls
+def eia_aeo_call(*, resp, year, **_):
+    """
+    Convert response for calling url to pandas dataframe, begin
+        parsing df into FBA format
+    :param resp: df, response from url call
+    :return: pandas dataframe of original source data
+    """
+    df_json = json.loads(resp.text)
+    # convert response to dataframe
+    series = df_json["series"]
+    df_series = pd.DataFrame(
+        data=series[1:len(series)], columns=series[0])
+    data = ["data"]
+    d = [pd.DataFrame(df_series[col].tolist()).add_prefix(col) for col in data]
+    df_series = pd.concat([df_series, d[0]], axis=1)
+    df_series = df_series.drop('data', axis=1)
+    row_1=df_series.iloc[0]
+    for col in df_series.columns:
+        if "data" in col:
+            # change the column name from the current data to the row 1 year value.
+            data = row_1[col]
+            year_str = data[0]
+            split_data = pd.DataFrame(df_series[col].to_list(), columns=[col + "_split", year_str])
+            df_series = pd.concat([df_series, split_data], axis=1)
+            df_series = df_series.drop(col, axis=1)
+            df_series = df_series.drop(col + "_split", axis=1)
+            if year_str != year:
+                df_series = df_series.drop(year_str, axis=1)
+    df_series = df_series.drop(columns=["updated", "end", "start", "f", "lastHistoricalPeriod", "description"])
+    df_series = df_series.rename(columns={"series_id": "Description"})
+    df_series = df_series.rename(columns={"units": "Unit"})
+    return df_series
+
+
+def eia_aeo_parse(*, df_list, year, **_):
+    """
+    Combine, parse, and format the provided dataframes
+    :param df_list: list of dataframes to concat and format
+    :param year: year
+    :return: df, parsed and partially formatted to
+        flowbyactivity specifications
+    """
+    # concat dataframes
+    df = pd.concat(df_list, sort=False)
+    # Add year
+    df['Year'] = year
+    df['Location'] = '00000'
+    df = df.rename(columns={year: "FlowAmount"})
+
+
+    for index, row in df.iterrows():
+        string_name = row["name"]
+        #split the string based on :
+        name_array = string_name.split(":")
+        if len(name_array) == 4:
+            name_string = name_array[0] + " " + name_array[1] + " " + name_array[2]
+            apb_stting = name_array[3]
+            df.loc[index, 'FlowName'] = name_string
+            df.loc[index, 'ActivityProducedBy'] = apb_stting
+        elif len(name_array) == 3:
+            name_string = name_array[0] + " " + name_array[1]
+            apb_stting = name_array[2]
+            df.loc[index, 'FlowName'] = name_string
+            df.loc[index, 'ActivityProducedBy'] = apb_stting
+        elif len(name_array) == 2:
+            name_string = name_array[0]
+            apb_stting = name_array[1]
+            df.loc[index, 'FlowName'] = name_string
+            df.loc[index, 'ActivityProducedBy'] = apb_stting
+        else:
+            print(name_array)
+    df = df.drop('name', axis=1)
+    # add location system based on year of data
+    df = assign_fips_location_system(df, year)
+    # hard code data
+    df['SourceName'] = 'EIA_AEO'
+    # Add tmp DQ scores
+    df['DataReliability'] = 5
+    df['DataCollection'] = 5
+    df['Compartment'] = None
+    df['Class'] = 'Energy'
+    return df
