1c39857e5815c436b3f9608c175305083ded3c4d
Eric Bell <eric.bell@erg.com>
2022-06-15 17:27:45 -0400

    add clean_fba_before_mapping_df_fxn to disaggregate emissions associated with ODS substitutes

--- flowsa/data_source_scripts/EPA_SIT.py
+++ flowsa/data_source_scripts/EPA_SIT.py
@@ -9,8 +9,10 @@ data directory. Parses EPA SIT data to flowbyactivity format.
 import pandas as pd
 import os
 from flowsa.settings import externaldatapath, log
-from flowsa.flowbyfunctions import assign_fips_location_system
+from flowsa.flowbyfunctions import assign_fips_location_system, \
+    load_fba_w_standardized_units
 from flowsa.location import apply_county_FIPS
+from flowsa.schema import flow_by_activity_fields
 
 def epa_sit_parse(*, source, year, config, **_):
 
@@ -114,6 +116,60 @@ def epa_sit_parse(*, source, year, config, **_):
     df0 = assign_fips_location_system(df0, '2015')
 
     return df0
+
+def disaggregate_emissions(fba, source_dict, **_):
+    """
+    clean_fba_before_mapping_df_fxn to assign specific flow names to flows
+    that are an amalgamation of multiple different GHGs
+    (e.g., "HFC, PFC, SF6, and NF3 emissions")
+    
+    """
+    
+    # dictionary of activities where GHG emissions need to be disaggregated
+    activity_dict = source_dict['clean_activity_dict']
+
+    # for all activities included in the dictionary...
+    for activity_name, activity_properties in activity_dict.items():
+        disaggregation_method = activity_properties.get('disaggregation_method')
+
+        # if emissions need to be split across multiple species, 
+        # allocate emissions proportionally based per the table listed
+        if disaggregation_method == 'table':
+            # name of table to be used for proportional split
+            table_name = activity_properties.get('disaggregation_data_source')
+            # load percentages to be used for proportional split
+            splits = load_fba_w_standardized_units(datasource=table_name,
+                year=source_dict['year'])
+            drop_rows = activity_properties.get('drop_rows')
+            # there are certain circumstances where one or more rows need to be 
+            # excluded from the table
+            if drop_rows is not None:
+                splits = splits[~splits['FlowName'].isin(drop_rows)]
+            splits['pct'] = splits['FlowAmount'] / splits['FlowAmount'].sum()
+            splits = splits[['FlowName', 'pct']]
+            # split fba dataframe to include only those items matching the activity
+            fba_activity = fba[fba['ActivityProducedBy'] == activity_name]
+            fba_main = fba[fba['ActivityProducedBy'] != activity_name]
+            # apply proportional split to activity data
+            speciated_df = fba_activity.apply(lambda x: [p * x['FlowAmount'] for p in splits['pct']],
+                            axis=1, result_type='expand')
+            speciated_df.columns = splits['FlowName']
+            speciated_df = pd.concat([fba_activity, speciated_df], axis=1)
+            speciated_df = speciated_df.melt(id_vars=flow_by_activity_fields.keys(),
+                                             var_name='Flow')
+            speciated_df['FlowName'] = speciated_df['Flow']
+            speciated_df['FlowAmount'] = speciated_df['value']
+            speciated_df.drop(columns=['Flow', 'value'], inplace=True)
+            # merge split dataframes back together
+            fba = pd.concat([fba_main, speciated_df], axis=0, join='inner')
+
+        # if emissions are attributable to only one species,
+        # allocate emissions directly to single species listed
+        elif disaggregation_method == 'direct':
+            species_name = activity_properties.get('disaggregation_data_source')
+            fba.loc[fba.ActivityProducedBy == activity_name, 'FlowName'] = species_name
+
+    return fba
 
 if __name__ == '__main__':
     import flowsa
