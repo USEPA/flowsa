debd01923dd0bd61b853d39091046315f68264fb
Ben Young <Ben.Young@erg.com>
2022-06-17 12:39:00 -0400

    add function to track inputs and outputs to processing

--- flowsa/data_source_scripts/EPA_CDDPath.py
+++ flowsa/data_source_scripts/EPA_CDDPath.py
@@ -12,8 +12,9 @@ Last updated: 2018-11-07
 import pandas as pd
 from flowsa.location import US_FIPS
 from flowsa.settings import externaldatapath
-from flowsa.flowbyfunctions import assign_fips_location_system
+from flowsa.flowbyfunctions import assign_fips_location_system, aggregator
 from flowsa.dataclean import standardize_units
+from flowsa.schema import flow_by_activity_mapped_fields
 
 
 # Read pdf into list of DataFrame
@@ -83,20 +84,15 @@ def epa_cddpath_parse(*, df_list, year, **_):
     return df
 
 
-def write_cdd_path_from_csv():
-    file = 'EPA_2016_Table5_CNHWCGenerationbySource_Extracted_' \
-           'UsingCNHWCPathNames.csv'
-    df = pd.read_csv(externaldatapath + file, header=0,
-                     names=['FlowName', 'ActivityProducedBy',
-                            'FlowAmount'])
-    return df
-
-
 def combine_cdd_path(*, resp, **_):
     """Call function to generate combined dataframe from csv file and
     excel dataset, applying the ActivityProducedBy across the flows.
     """
-    df_csv = write_cdd_path_from_csv()
+    file = 'EPA_2016_Table5_CNHWCGenerationbySource_Extracted_' \
+           'UsingCNHWCPathNames.csv'
+    df_csv = pd.read_csv(externaldatapath + file, header=0,
+                         names=['FlowName', 'ActivityProducedBy',
+                                'FlowAmount'])
     df_csv['pct'] = (df_csv['FlowAmount']/
                      df_csv.groupby(['FlowName'])
                      .FlowAmount.transform('sum'))
@@ -129,3 +125,30 @@ def assign_wood_to_engineering(df):
         df = standardize_units(df)
 
     return df
+
+
+def cdd_processing(df):
+    material = 'Concrete'
+    inputs = df.loc[df['FlowName'] == material]
+    inputs = inputs.reset_index(drop=True)
+    ouptuts = inputs.copy()
+    inputs['ActivityConsumedBy'] = f"{material} Processing"
+    ouptuts['ActivityProducedBy'] = f"{material} Processing"
+
+    df1 = pd.concat([inputs, ouptuts], ignore_index=True)
+    cols = flow_by_activity_mapped_fields.copy()
+    cols.pop('FlowAmount')
+    df1 = aggregator(df1, cols)
+
+    df1.loc[df1['ActivityConsumedBy'] == 'Remanufacture',
+            'ActivityConsumedBy'] = f'Remanufacture - {material}'
+
+    return df1
+
+if __name__ == "__main__":
+    import flowsa
+    # flowsa.flowbyactivity.main(source='EPA_CDDPath', year=2014)
+    fba = flowsa.getFlowByActivity(datasource='EPA_CDDPath', year=2014)
+
+    flowsa.flowbysector.main(method='CDD_concrete_national_2014')
+    fbs = flowsa.getFlowBySector(methodname='CDD_concrete_national_2014')
