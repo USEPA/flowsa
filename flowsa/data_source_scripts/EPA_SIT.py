66d2285d5072bbfa371ca5aa90c7b432334ad4b2
Eric Bell <eric.bell@erg.com>
2022-08-17 15:41:40 -0400

    Revise EPA_SIT to process multiple states in one run.

--- flowsa/data_source_scripts/EPA_SIT.py
+++ flowsa/data_source_scripts/EPA_SIT.py
@@ -16,93 +16,96 @@ from flowsa.schema import flow_by_activity_fields
 
 def epa_sit_parse(*, source, year, config, **_):
 
-    state = config['state']
-    
     # initialize the dataframe
-    df0 = pd.DataFrame()
-    
-    # for each Excel data file listed in the .yaml...
-    for file in config['files']:
-        print(f'Loading data from {file}...')
-        filepath = f"{externaldatapath}/SIT_data/{state}/{file}"
-        # dictionary containing Excel sheet-specific information
-        file_dict = config['files'][file]['file_dict']
+    df0 = pd.DataFrame()   
 
-        if not os.path.exists(filepath):
-            raise FileNotFoundError(f'SIT file not found in {filepath}')
+    # for each state listed in the method file...
+    for state in config['state_list']:
+        log.info(f'Parsing data for {state}...')
+        
+        # for each Excel data file listed in the .yaml...
+        for file in config['files']:
+            log.info(f'Loading data from {file}...')
+            filepath = f"{externaldatapath}/SIT_data/{state}/{file}"
+            # dictionary containing Excel sheet-specific information
+            file_dict = config['files'][file]['file_dict']
     
-        # for each sheet in the Excel file containing data...
-        for sheet, sheet_dict in file_dict.items():
-            sheetname = sheet_dict.get('sheetname', sheet)
-            tablename = sheet_dict.get('tablename')
-            if tablename:
-                sheetandtable = f'{sheetname}, {tablename}'
-            else:
-                sheetandtable = sheetname
-            tablename = sheet_dict.get('tablename', sheetname)
-            log.debug(f'Loading data from: {sheetname}...')
-            # read in data from Excel sheet
-            df = pd.read_excel(filepath,
-                               sheet_name = sheetname,
-                               header=sheet_dict.get('header', 2),
-                               skiprows=range(sheet_dict.get('skiprowstart', 0),
-                                              sheet_dict.get('skiprowend', 0)),
-                               usecols="B:AG",
-                               nrows=sheet_dict.get('nrows'))
-            df.columns = df.columns.map(str)
-            df['ActivityProducedBy'] = df.iloc[:,0]
-    
-            # for each row in the data table...
-            # ...emissions categories will be renamed with the format
-            # 'sheet name, emissions category'
-            # ...emissions subcategories will be renamed with the format
-            # 'sheet name, emissions category, emissions subcategory'
-            for ind in df.index:
-                current_header = df['ActivityProducedBy'][ind].strip()
-                # for level 1 headers...
-                if current_header in sheet_dict.get('headers'):
-                    active_header = current_header
-                    if sheet_dict.get('subgroup') == 'activitybyflow':
-                        df.loc[ind, 'FlowName'] = active_header
-                    elif sheet_dict.get('subgroup') == 'flow':
-                        df.loc[ind, 'FlowName'] = 'Total N2O and CH4 Emissions'
-                    df.loc[ind,'ActivityProducedBy'] = (
-                        f'{sheetandtable}, {active_header}')
-                # for level 2 headers...
-                elif current_header not in sheet_dict.get('subsubheaders',''):
-                    active_subheader = df['ActivityProducedBy'][ind].strip()
-                    if sheet_dict.get('subgroup') == 'flow':
-                        df.loc[ind, 'FlowName'] = active_subheader
+            if not os.path.exists(filepath):
+                raise FileNotFoundError(f'SIT file not found in {filepath}')
+        
+            # for each sheet in the Excel file containing data...
+            for sheet, sheet_dict in file_dict.items():
+                sheetname = sheet_dict.get('sheetname', sheet)
+                tablename = sheet_dict.get('tablename')
+                if tablename:
+                    sheetandtable = f'{sheetname}, {tablename}'
+                else:
+                    sheetandtable = sheetname
+                tablename = sheet_dict.get('tablename', sheetname)
+                log.debug(f'Loading data from: {sheetname}...')
+                # read in data from Excel sheet
+                df = pd.read_excel(filepath,
+                                   sheet_name = sheetname,
+                                   header=sheet_dict.get('header', 2),
+                                   skiprows=range(sheet_dict.get('skiprowstart', 0),
+                                                  sheet_dict.get('skiprowend', 0)),
+                                   usecols="B:AG",
+                                   nrows=sheet_dict.get('nrows'))
+                df.columns = df.columns.map(str)
+                df['ActivityProducedBy'] = df.iloc[:,0]
+        
+                # for each row in the data table...
+                # ...emissions categories will be renamed with the format
+                # 'sheet name, emissions category'
+                # ...emissions subcategories will be renamed with the format
+                # 'sheet name, emissions category, emissions subcategory'
+                for ind in df.index:
+                    current_header = df['ActivityProducedBy'][ind].strip()
+                    # for level 1 headers...
+                    if current_header in sheet_dict.get('headers'):
+                        active_header = current_header
+                        if sheet_dict.get('subgroup') == 'activitybyflow':
+                            df.loc[ind, 'FlowName'] = active_header
+                        elif sheet_dict.get('subgroup') == 'flow':
+                            df.loc[ind, 'FlowName'] = 'Total N2O and CH4 Emissions'
                         df.loc[ind,'ActivityProducedBy'] = (
                             f'{sheetandtable}, {active_header}')
-                    elif sheet_dict.get('subgroup') == 'activitybyflow':
-                        df.loc[ind, 'FlowName'] = active_header
-                        df.loc[ind,'ActivityProducedBy'] = (
-                            f'{sheetandtable}, {active_subheader}')
+                    # for level 2 headers...
+                    elif current_header not in sheet_dict.get('subsubheaders',''):
+                        active_subheader = df['ActivityProducedBy'][ind].strip()
+                        if sheet_dict.get('subgroup') == 'flow':
+                            df.loc[ind, 'FlowName'] = active_subheader
+                            df.loc[ind,'ActivityProducedBy'] = (
+                                f'{sheetandtable}, {active_header}')
+                        elif sheet_dict.get('subgroup') == 'activitybyflow':
+                            df.loc[ind, 'FlowName'] = active_header
+                            df.loc[ind,'ActivityProducedBy'] = (
+                                f'{sheetandtable}, {active_subheader}')
+                        else:
+                            df.loc[ind,'ActivityProducedBy'] = (
+                                f'{sheetandtable}, {active_header}, '
+                                f'{active_subheader}')
+                    # for level 3 headers (only occur in IndirectCO2 and Agriculture tabs)...
                     else:
+                        subsubheader = df['ActivityProducedBy'][ind].strip()
                         df.loc[ind,'ActivityProducedBy'] = (
                             f'{sheetandtable}, {active_header}, '
-                            f'{active_subheader}')
-                # for level 3 headers (only occur in IndirectCO2 and Agriculture tabs)...
-                else:
-                    subsubheader = df['ActivityProducedBy'][ind].strip()
-                    df.loc[ind,'ActivityProducedBy'] = (
-                        f'{sheetandtable}, {active_header}, '
-                        f'{active_subheader}, {subsubheader}')
-    
-            # drop all columns except the desired emissions year and the
-            # emissions activity source
-            df = df.filter([year, 'ActivityProducedBy', 'FlowName'])
-            # rename columns
-            df = df.rename(columns={year: 'FlowAmount'})
-            # add sheet-specific hardcoded data
-            if 'subgroup' not in sheet_dict:
-                df['FlowName'] = sheet_dict.get('flow')
-            df['Unit'] = sheet_dict.get('unit')
-            df['Description'] = sheet
-    
-            # concatenate dataframe from each sheet with existing master dataframe
-            df0 = pd.concat([df0, df])
+                            f'{active_subheader}, {subsubheader}')
+        
+                # drop all columns except the desired emissions year and the
+                # emissions activity source
+                df = df.filter([year, 'ActivityProducedBy', 'FlowName'])
+                # rename columns
+                df = df.rename(columns={year: 'FlowAmount'})
+                # add sheet-specific hardcoded data
+                if 'subgroup' not in sheet_dict:
+                    df['FlowName'] = sheet_dict.get('flow')
+                df['Unit'] = sheet_dict.get('unit')
+                df['Description'] = sheet
+                df['State'] = state
+        
+                # concatenate dataframe from each sheet with existing master dataframe
+                df0 = pd.concat([df0, df])
 
     # add general hardcoded data
     df0['Class'] = 'Chemicals'
@@ -114,7 +117,6 @@ def epa_sit_parse(*, source, year, config, **_):
     df0['DataCollection'] = 5
 
     # add state FIPS code
-    df0['State'] = state
     df0['County'] = ''
     df0 = apply_county_FIPS(df0, year='2015', source_state_abbrev=True)
     # add FIPS location system
@@ -178,5 +180,5 @@ def disaggregate_emissions(fba, source_dict, **_):
 
 if __name__ == '__main__':
     import flowsa
-    flowsa.flowbyactivity.main(source='EPA_SIT', year='2017')
-    fba = flowsa.getFlowByActivity('EPA_SIT', '2017')
+    flowsa.flowbyactivity.main(source='EPA_SIT', year='2019')
+    fba = flowsa.getFlowByActivity('EPA_SIT', '2019')
