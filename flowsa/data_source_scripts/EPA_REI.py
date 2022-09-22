ca6fa88da00fb654fba0e343b33ad24088b1ad8d
catherinebirney <birney.catherine@epa.gov>
2022-09-22 15:55:28 -0400

    complete REI primary flow and waste flow fbas

--- flowsa/data_source_scripts/EPA_REI.py
+++ flowsa/data_source_scripts/EPA_REI.py
@@ -21,7 +21,7 @@ def rei_call(*, url, **_):
     return df
 
 
-def rei_parse(*, df_list, year, **_):
+def primary_factors_parse(*, df_list, year, **_):
     """
     Combine, parse, and format the provided dataframes
     :param df_list: list of dataframes to concat and format
@@ -29,9 +29,6 @@ def rei_parse(*, df_list, year, **_):
     :return: df, parsed and partially formatted to
         flowbyactivity specifications
     """
-    # create list of dataframes for primary factors and waste
-    rei_list = []
-
     # df for primary factors
     df = pd.concat(df_list, sort=False)
     df.iloc[0, 0] = 'Description'
@@ -51,16 +48,51 @@ def rei_parse(*, df_list, year, **_):
     df['Unit'] = 'Thousand USD'
     df.loc[df['FlowName'] == 'Employment', 'Unit'] = 'p'
     df["Year"] = year
-    df['DataReliability'] = 5  # tmp
-    df['DataCollection'] = 5  # tmp
+    df['DataReliability'] = 5
+    df['DataCollection'] = 5
 
-    rei_list.append(df)
+    return df
 
-    # waste flows only
-    df1 = df.query('Description.str.startswith("RS")',
-                   engine='python').reset_index(drop=True)
-    df1 = df1.assign(SourceName='EPA_REI_WF')
 
-    rei_list.append(df1)
+def waste_parse(*, df_list, year, **_):
+    """
+    Combine, parse, and format the provided dataframes
+    :param df_list: list of dataframes to concat and format
+    :param year: year of FBS
+    :return: df, parsed and partially formatted to
+        flowbyactivity specifications
+    """
+    # df for primary factors
+    df = pd.concat(df_list, sort=False)
+    df.iloc[0, 0] = 'Description'
+    df.iloc[0, 1] = 'FlowName'
+    df = df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True)
 
-    return rei_list
+    df = (df
+          .drop(columns=df.columns[[2]])
+          .drop([0, 1])  # Drop blanks
+          .melt(id_vars=['Description', 'FlowName'],
+                var_name='ActivityConsumedBy',
+                value_name='FlowAmount')
+          )
+    df = df[~df['FlowAmount'].str.contains('-')].reset_index(
+        drop=True)
+    df['FlowAmount'] = (df['FlowAmount'].str.replace(
+        ',', '').astype('float'))
+    df['Unit'] = 'USD'
+    # Where recyclable code >= 9 (Gleaned product), change units to MT
+    df.loc[df['Description'].str[-2:].astype('int') >= 9,
+           'Unit'] = 'MT'
+    df['FlowName'] = df['FlowName'].apply(
+        lambda x: x.split('(', 1)[0])
+
+    # hardcode info
+    df['Location'] = US_FIPS
+    df['SourceName'] = 'EPA_REI_W'
+    df["Class"] = "Other"
+    df['FlowType'] = 'WASTE_FLOW'
+    df["Year"] = year
+    df['DataReliability'] = 5
+    df['DataCollection'] = 5
+
+    return df
