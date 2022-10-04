c595e5bc4c3072cb227763bd50de4b7fca933e40
Ben Young <Ben.Young@erg.com>
2022-10-04 11:23:18 -0400

    add action to enable user triggered FBS comparison

--- flowsa/test_FBS_against_remote.py
+++ flowsa/test_FBS_against_remote.py
@@ -4,11 +4,10 @@ Tests to run during github action
 import pytest
 import os
 from flowsa import seeAvailableFlowByModels
-from flowsa.metadata import set_fb_meta
-from flowsa.settings import paths, diffpath
-from flowsa.validation import compare_FBS_results
+from flowsa.settings import diffpath
 from flowsa.common import check_method_status
-from esupy.processed_data_mgmt import download_from_remote
+from flowsa.test_single_FBS import compare_single_FBS_against_remote
+
 
 @pytest.mark.skip(reason="Perform targeted test for compare_FBS on PR")
 def test_FBS_against_remote():
@@ -19,27 +18,12 @@ def test_FBS_against_remote():
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     for m in seeAvailableFlowByModels("FBS", print_method=False):
-        status = download_from_remote(set_fb_meta(m, "FlowBySector"),
-                                      paths)
-        if not status:
-            print(f"{m} not found in remote server. Skipping...")
-            continue
         if method_status.get(m) is not None:
             print(f"{m} skipped due to "
                   f"{method_status.get(m).get('Status', 'Unknown')}")
             continue
-        print("--------------------------------\n"
-              f"Method: {m}\n"
-              "--------------------------------")
-        df = compare_FBS_results(m, m, compare_to_remote=True)
-        df.rename(columns = {'FlowAmount_fbs1': 'FlowAmount_remote',
-                             'FlowAmount_fbs2': 'FlowAmount_HEAD'},
-                  inplace=True)
-        if len(df) > 0:
-            print(f"Saving differences in {m} to csv")
-            df.to_csv(f"{outdir}{m}_diff.csv", index=False)
-        else:
-            print(f"***No differences found in {m}***")
+        compare_single_FBS_against_remote(m)
+
 
 if __name__ == "__main__":
     test_FBS_against_remote()
