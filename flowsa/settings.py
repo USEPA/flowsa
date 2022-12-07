b838300fe199a93cb5c9c7e73945513de2bfc791
Ben Young <Ben.Young@erg.com>
2022-12-06 22:51:29 -0500

    reversion of 61c8e38b, except use `version('flowsa')` as backup

--- flowsa/settings.py
+++ flowsa/settings.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import logging
+import subprocess
 from importlib.metadata import version
 from pathlib import Path
 from esupy.processed_data_mgmt import Paths, create_paths_if_missing
@@ -98,8 +99,19 @@ vLogDetailed.addHandler(vLog_fh)
 
 
 def return_pkg_version():
-    # As of Python 3.8 can use the following:
-    return version('flowsa')
+    # return version with git describe
+    try:
+        # set path to flowsa repository, necessary if running method files
+        # outside the flowsa repo
+        tags = subprocess.check_output(
+            ["git", "describe", "--tags", "--always"],
+            cwd=MODULEPATH).decode().strip()
+        pkg_version = tags.split("-", 1)[0].replace('v', "")
+    except subprocess.CalledProcessError:
+        log.info('Unable to return version with git describe')
+        pkg_version = version('flowsa')
+
+    return pkg_version
 
 
 # https://stackoverflow.com/a/41125461
