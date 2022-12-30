033359dbabb700019be3a31d231c69f998d09969
catherinebirney <birney.catherine@epa.gov>
2022-12-30 12:56:48 -0500

    for clean_qcew_for_fbs() do not want to include clean_qcew() because it converts 5 digit NAICS to 6 digits by adding a 0. no longer need to do this because we are first running fxn to estimate suppressed data

--- flowsa/data_source_scripts/temp_data_source_functions.py
+++ flowsa/data_source_scripts/temp_data_source_functions.py
@@ -62,17 +62,8 @@ def clean_qcew_for_fbs(fba: FlowByActivity, **kwargs):
     dictionary of FBA method yaml parameters
     :return: df, BLS QCEW FBA with estimated suppressed data
     """
-    df = clean_qcew(fba, **kwargs)
-
-    # for purposes of allocation, we do not need to differentiate between
-    # federal government, state government, local government, or private
-    # sectors. So after estimating the suppressed data (above), modify the
-    # flow names and aggregate data
-    df['Flowable'] = (df['Flowable'].str.split(',').str[0]
-                      .str.replace('Number of employees', 'Jobs'))
-    df2 = df.aggregate_flowby()
-
-    return df2
+    fba['Flowable'] = 'Jobs'
+    return fba
 
 
 def estimate_suppressed_qcew(fba: FlowByActivity) -> FlowByActivity:
