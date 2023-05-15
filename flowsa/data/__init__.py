173a0faa8c4e316be09c2b3a1719e106b931f23b
catherinebirney <birney.catherine@epa.gov>
2023-05-15 10:46:34 -0600

    read crosswalk columns as dtype object - otherwise sectors can be imported as int and cause merge errors

--- flowsa/data/__init__.py
+++ flowsa/data/__init__.py
@@ -5,5 +5,7 @@ import pandas as pd
 def crosswalk(
     crosswalk: str,
 ) -> pd.DataFrame:
+    # read crosswalk columns as dtype object - otherwise sectors can be 
+    # imported as int and cause merge errors
     return pd.read_csv(files(f'{__package__}.activitytosectormapping')
-                       .joinpath(f'{crosswalk}.csv'))
+                       .joinpath(f'{crosswalk}.csv'), dtype=object)
