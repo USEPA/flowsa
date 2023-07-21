84a223e47290da78df59b7c85c9390f86b061ec3
catherinebirney <birney.catherine@epa.gov>
2023-07-21 16:45:51 -0600

    move and rename clean_mecs_energy_fba() and clean_mapped_mecs_energy_fba()

--- flowsa/flowbyclean.py
+++ flowsa/flowbyclean.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
-from flowsa.flowby import FlowByActivity, FlowBySector, FB
+from flowsa.flowby import FlowBySector, FB, get_catalog_info, \
+    get_flowby_from_config
 from flowsa.flowsa_log import log
-from flowsa import (flowby, geo, location, getFlowBySector, flowbyfunctions)
+from flowsa import (geo, location, getFlowBySector, flowbyfunctions,
+                    FlowByActivity)
 from flowsa.naics import map_source_sectors_to_less_aggregated_sectors
 
 def return_primary_activity_column(fba: FlowByActivity) -> \
@@ -35,12 +37,12 @@ def load_prepare_clean_source(
     except AttributeError:
         name, config = self.config['clean_source'], {}
 
-    clean_fbs = flowby.get_flowby_from_config(
+    clean_fbs = get_flowby_from_config(
         name=name,
         config={**{k: v for k, v in self.config.items()
                    if k in self.config['method_config_keys']
                    or k == 'method_config_keys'},
-                **flowby.get_catalog_info(name),
+                **get_catalog_info(name),
                 **config},
         download_sources_ok=download_sources_ok
         ).prepare_fbs(download_sources_ok=download_sources_ok)
@@ -365,3 +367,133 @@ def calculate_flow_per_employee(
             )
 
     return fbs
+
+
+def define_parentincompletechild_descendants(
+        fba: FlowByActivity, activity_col='ActivityConsumedBy', **_) -> \
+        FlowByActivity:
+    '''
+    This function helps address the unique structure of the EIA MECS dataset.
+    The MECS dataset contains rows at various levels of aggregation between
+    NAICS-3 and NAICS-6 (inclusive). Each aggregated row contains the total
+    for that level of aggregation, even if data are also reported for a less
+    aggregated subset of those industries. For example:
+
+    ActivityConsumedBy | FlowAmount | ...
+    -------------------------------------
+    311                | 110        |
+    3112               |  65        |
+    311221             |  55        |
+
+    where the 110 reported for 311 includes the 65 reported for 3112, which
+    includes the 55 reported for 211221. If we do not address this issue, there
+    will be double counting. Additionally, if we are trying to disaggregate
+    to the NAICS-6 level, all three rows shown above will be mapped to NAICS-6
+    311221 (with the first wo rows also being mapped to several other NAICS-6
+    codes as well). We will then over attribute the (double-counted) flows to
+    those industries and groups of industries for which more specific detail
+    is provided.
+
+    This function addresses the double counting issue. For each aggregated
+    industry group, all descendant (less aggregated) industries or industry
+    groups for which detailed information is given are subtracted from the
+    aggregated total. Using the example from above:
+
+    ActivityConsumedBy | FlowAmount | ...
+    -------------------------------------
+    311                |  45        |
+    3112               |  10        |
+    311221             |  55        |
+
+    Additionally, this function adds a column called "descendants", which for
+    each industry holds all the descendant industries or industry groups that
+    have detailed information provided in the dataset. After mapping to
+    industries, but before attribution is performed, this column is used by the
+    drop_parentincompletechild_descendants function to drop any row that is mapped
+    from an aggregated industry group to a less aggregated industry or industry
+    group THAT HAS DETAILED INFORMATION GIVEN IN THE MECS (and therefore has
+    its own row already) to avoid the over-attribution issue.
+    Again using the previous example:
+
+    ActivityConsumedBy | FlowAmount | descendants | ...
+    ---------------------------------------------------
+    311                |  45        | 3112 311221 |
+    3112               |  10        | 311221      |
+    311221             |  55        |             |
+
+    Note that this function is not useful if the desired aggregation level is
+    NAICS-2. In such a case, the MECS dataset can be filtered to include only
+    the rows with ActivityConsumedBy == "31-33", then disaggregated to 31, 32,
+    33 using another dataset (such as the QCEW).
+    '''
+    fba = (
+        fba
+        .query(f'{activity_col} != "31-33"')
+        .assign(descendants='')
+    )
+
+    for level in [5, 4, 3]:
+        descendants = (
+            fba
+            .drop(columns='descendants')
+            .query(f'{activity_col}.str.len() > {level}')
+            .assign(
+                parent=lambda x: x[activity_col].str.slice(stop=level)
+            )
+            .groupby(['Flowable', 'Location', 'parent'])
+            .agg({'FlowAmount': 'sum', activity_col: ' '.join})
+            .reset_index()
+            .rename(columns={activity_col: 'descendants',
+                             'FlowAmount': 'descendant_flows',
+                             'parent': 'ActivityConsumedBy'})
+        )
+
+        fba = (
+            fba
+            .merge(descendants,
+                   how='left',
+                   on=['Flowable', 'Location', 'ActivityConsumedBy'],
+                   suffixes=(None, '_y'))
+            .fillna({'descendant_flows': 0, 'descendants_y': ''})
+            .assign(
+                descendants=lambda x: x.descendants.mask(x.descendants == '',
+                                                         x.descendants_y),
+                FlowAmount=lambda x: (x.FlowAmount -
+                                      x.descendant_flows).mask(
+                    x.FlowAmount - x.descendant_flows < 0, 0)
+            )
+            .drop(columns=['descendant_flows', 'descendants_y'])
+        )
+
+    return fba
+
+
+def drop_parentincompletechild_descendants(
+        fba: FlowByActivity, sector_col='SectorConsumedBy', **_) -> \
+        FlowByActivity:
+    '''
+    This function finishes handling the over-attribution issue described in
+    the documentation for define_parentincompletechild_descendants by dropping any row in the
+    MECS dataset which has been mapped to an industry or industry group which
+    is a subset (strict or otherwise) of an industry group listed in the
+    descendants columns. So, if 311 and 3112 both appear in the MECS datset,
+    3112 will be listed as a descendant of 311 and this function will therefore
+    drop a row mapping 311 to 311221 (since more detailed information on 3112,
+    which contains 311221, is provided). If 31122 and 311221 do not appear in
+    the dataset, a row mapping 3112 to 311221 will not be dropped, since no
+    more detailed information on 311221 is given. Further attribution/
+    disaggregation should be done using another datatset such as the QCEW.
+    '''
+
+    fba2 = (
+        fba
+        .assign(to_keep=fba.apply(
+            lambda x: not any([str(x[sector_col]).startswith(d) for d in
+                               x.descendants.split()]),
+            axis='columns'
+        ))
+        .query('to_keep')
+        .drop(columns=['descendants', 'to_keep'])
+    )
+
+    return fba2
