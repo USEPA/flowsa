d9bd436202652cec876cdf0c52a8137d888fcbf4
catherinebirney <birney.catherine@epa.gov>
2023-09-26 13:46:12 -0600

    expand on text explanation in FBS examples

--- examples/get_flows_by_sector.py
+++ examples/get_flows_by_sector.py
@@ -11,7 +11,9 @@ For example, Water_national_2015_m1 (https://github.com/USEPA/flowsa/blob/
 master/flowsa/methods/flowbysectormethods/Water_national_2015_m1.yaml)
 captures the flow of withdrawn water through the economy. This dataset
 tracks water withdrawn by Public Supply (SectorProducedBy) that flows to
-Domestic use (SectorConsumedBy). Not all FBS contain data in both the
+Domestic use (SectorConsumedBy).
+
+Not all FBS contain data in both the
 SectorProducedBy and SectorConsumedBy columns. For example
 Employment_national_2018 (https://github.com/USEPA/flowsa/blob/master/
 flowsa/methods/flowbysectormethods/Employment_national_2018.yaml) only contains
@@ -21,9 +23,16 @@ employment between sectors.
 Tables are standardized into a table format defined in
 https://github.com/USEPA/flowsa/blob/master/format%20specs/FlowBySector.md.
 
-Retrieves stored data in the FlowBySector format
-    :param methodname: string, Name of an available method for the given class.
-    Method files found in flowsa/data/flowbysectormethods
+`getFlowBySector()` has required and optional parameters
+    :param methodname: str, name of an available method
+    :param fbsconfigpath: str, path to the FBS method file if loading a file
+        from outside the flowsa repository, optional
+    :param download_FBAs_if_missing: bool, if True will attempt to load FBAs
+        used in generating the FBS from remote server prior to generating if
+        file not found locally, optional, default is False
+    :param download_FBS_if_missing: bool, if True will attempt to load from
+        remote server prior to generating if file not found locally,
+        optional, default is False
     :return: dataframe in flow by sector format
 
 """
@@ -33,10 +42,15 @@ from flowsa import getFlowBySector, collapse_FlowBySector, \
 # see available FBS models
 seeAvailableFlowByModels('FBS')
 
-# load FBS from local directory, if does not exist, method will download
+# load FBS from local directory, if does not exist, method will run, option
+# to download the FBAs from Data Commons
+# (https://dmap-data-commons-ord.s3.amazonaws.com/index.html?prefix=flowsa/)
+# to run the method
 fbs_water = getFlowBySector('Water_national_2015_m1',
                             download_FBAs_if_missing=True)
 
 # collapse the FBS - output has 'Sector' column instead of
-# 'SectorProducedBy' and 'SectorConsumedBy' columns
+# 'SectorProducedBy' and 'SectorConsumedBy' columns. The collapsed
+# `Water_national_2015_m1` FBS will have 2 fewer rows, as the df is aggregated
+# after dropping "SectorProducedBy" information
 fbs_water_collapsed = collapse_FlowBySector('Water_national_2015_m1')
