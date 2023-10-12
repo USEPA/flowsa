39a0f87d7afe5abc89a42fbc83cd58328cc88011
Ben Young <Ben.Young@erg.com>
2023-10-12 10:01:18 -0400

    consolidate scripts from merge conflict replicate c93e7a0

--- scripts/write_BEA_data_from_useeior.py
+++ scripts/write_BEA_data_from_useeior.py
@@ -3,17 +3,14 @@
 # coding=utf-8
 
 """
-A script to get Use table transactions from a useeior EEIOmodel.
-
-- Store them as .csv
-- Depends on rpy2 and tzlocal as well as having R installed and useeior installed.
-
+BEA data are imported from useeior and stored as csv files in 'external_data'
+folder of flowsa
 
 ---
-The BEA_Detail_Use_PRO_BeforeRedef_2012 was pulled from
-USEEIOR's Detail_Use_2012_PRO_BeforeRedef.rda on 08/11/2022.
+The BEA_Detail_Use_PRO_BeforeRedef was pulled from
+USEEIOR's Detail_Use_YEAR_PRO_BeforeRedef.rda on 09/06/2023.
 
-The original file is found here:
+One of the original files is found here:
 https://github.com/USEPA/useeior/blob/master/data/Detail_Use_2012_PRO_BeforeRedef.rda
 
 csv obtained by running the following code in Rstudio:
@@ -21,8 +18,6 @@ bea <- get('Detail_Use_2012_PRO_BeforeRedef')
 write.csv(bea, file='BEA_Detail_Use_2012_PRO_BeforeRedef.csv')
 
 CSV manually added to flowsa
-
----
 
 ---
 The Detail_Use_SUT_2017 was pulled from
@@ -39,11 +34,10 @@ The metadata file 'Detail_Use_SUT_2017_metadata.json' was manually copied from u
 
 ---
 
+The BEA_YEAR_Detail_Make_BeforeRedef was pulled from USEEIOR's
+Detail_Make_YEAR_BeforeRedef.rda on 09/06/2023.
 
-The BEA_2012_Detail_Make_BeforeRedef was pulled from USEEIOR's
-Detail_Make_2012_BeforeRedef.rda on 08/11/2022.
-
-The original file is found here:
+One of the original files is found here:
 https://github.com/USEPA/useeior/blob/master/data/Detail_Make_2012_BeforeRedef.rda
 
 csv obtained by running the following code in Rstudio:
@@ -51,10 +45,6 @@ bea <- get('Detail_Make_2012_BeforeRedef')
 write.csv(bea, file='BEA_Detail_Make_2012_BeforeRedef.csv')
 
 CSV manually added to flowsa
-
-https://github.com/USEPA/useeior/commit/2eece7a
-"date_last_modified": ["2021-09-29"],
-"date_accessed": ["2022-07-27"]
 
 ---
 The Detail_Supply_2017 was pulled from
@@ -72,7 +62,7 @@ The metadata file 'Detail_Supply_2017_metadata.json' was manually copied from us
 
 ---
 The BEA_Detail_GrossOutput was pulled from
-USEEIOR's Detail_GrossOutput_IO.rda on 08/11/2022.
+USEEIOR's Detail_GrossOutput_IO.rda on 09/06/2023.
 
 The original file is found here:
 https://github.com/USEPA/useeior/blob/master/data/Detail_GrossOutput_IO.rda
@@ -81,34 +71,32 @@ csv obtained by running the following code in Rstudio:
 bea <- get('Detail_GrossOutput_IO')
 write.csv(bea, file='BEA_Detail_GrossOutput_IO.csv')
 
-https://github.com/USEPA/useeior/commit/2eece7a
-"date_last_modified": ["2021-10-04"],
-"date_accessed": ["2022-07-27"]
-
 CSV manually added to flowsa
-
 
 """
 
-from rpy2.robjects.packages import importr
-from rpy2.robjects import pandas2ri
-from flowsa.settings import externaldatapath
+# package to read r code no longer working, so reverting to manually
+# importing BEA data from useeior
 
-pandas2ri.activate()
+# from rpy2.robjects.packages import importr
+# from rpy2.robjects import pandas2ri
+# from flowsa.settings import externaldatapath
 
-useeior = importr('useeior')
+# pandas2ri.activate()
 
-model_with_detail_2012_tables = 'USEEIOv2.0-GHG'
-model = useeior.buildEEIOModel(model_with_detail_2012_tables)
+# useeior = importr('useeior')
 
-# Get the UseTransactions object embedded within the BEA data
-UseIndustryTransactions = model.rx2("BEA").rx2("UseTransactions")
-# Convert to a pandas dataframe
-UseIndustryTransactions = pandas2ri.ri2py_dataframe(UseIndustryTransactions)
+# model_with_detail_2012_tables = 'USEEIOv2.0-GHG'
+# model = useeior.buildEEIOModel(model_with_detail_2012_tables)
 
-# Get the vector of model industries
-Industries = model.rx2("BEA").rx2("Industries")
-# Apply it to the df index
-UseIndustryTransactions.index = Industries
-# Write out to csv
-UseIndustryTransactions.to_csv(f"{externaldatapath}/BEA_2012_Detail_Use_Industry_Transactions.csv")
+# # Get the UseTransactions object embedded within the BEA data
+# UseIndustryTransactions = model.rx2("BEA").rx2("UseTransactions")
+# # Convert to a pandas dataframe
+# UseIndustryTransactions = pandas2ri.ri2py_dataframe(UseIndustryTransactions)
+
+# # Get the vector of model industries
+# Industries = model.rx2("BEA").rx2("Industries")
+# # Apply it to the df index
+# UseIndustryTransactions.index = Industries
+# # Write out to csv
+# UseIndustryTransactions.to_csv(f"{externaldatapath}/BEA_2012_Detail_Use_Industry_Transactions.csv")
