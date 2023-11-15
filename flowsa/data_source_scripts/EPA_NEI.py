2d01dfc4a6111ee9d3208be599a878c17f266803
Ben Young <Ben.Young@erg.com>
2023-11-15 16:30:09 -0500

    add back in convert to geoscale

--- flowsa/data_source_scripts/EPA_NEI.py
+++ flowsa/data_source_scripts/EPA_NEI.py
@@ -201,6 +201,9 @@ def clean_NEI_fba(fba: FlowByActivity, **_) -> FlowByActivity:
     new_fba = FlowByActivity(fba)
     for attr in attributes_to_save:
         setattr(new_fba, attr, attributes_to_save[attr])
+    # to reduce the file size of the FBA and avoid memory errors, consolidate
+    # to geoscale (i.e., state) early on
+    new_fba = new_fba.convert_to_geoscale()
 
     return new_fba
 
