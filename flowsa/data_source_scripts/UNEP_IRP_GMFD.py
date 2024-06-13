f8b308f66c22c3f403ce52b4e18ca8d327acf77f
Ben Young <Ben.Young@erg.com>
2024-06-13 16:22:52 -0400

    adjust call function and linting

--- flowsa/data_source_scripts/UNEP_IRP_GMFD.py
+++ flowsa/data_source_scripts/UNEP_IRP_GMFD.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
+# UNEP_IRP_GMFD.py (flowsa)
+# !/usr/bin/env python3
+# coding=utf-8
 """
 Global Material Flows Database
 https://unep-irp.fineprint.global/mfa13/export
@@ -8,21 +9,19 @@ https://unep-irp.fineprint.global/mfa13/export
 
 import io
 import pandas as pd
-import numpy as np
 from flowsa.flowbyfunctions import assign_fips_location_system
 from flowsa.flowsa_log import log
-from flowsa.flowbyactivity import FlowByActivity
 
 
-def unep_mfa_call(*, resp, url,  **_):
+def unep_mfa_call(*, resp, config,  **_):
     """
     Convert response for calling url to pandas dataframe, begin
         parsing df into FBA format
     :param resp: df, response from url call
     :return: pandas dataframe of original source data
     """
-    df = pd.read_csv('https://unep-irp.fineprint.global/mfa13/export',skiprows=0,
-    skipfooter=0)
+    with io.StringIO(resp.text) as f:
+        df = pd.read_csv(f)
     return df
 
 def unep_mfa_parse(*, df_list, year, config, **_):
@@ -33,30 +32,33 @@ def unep_mfa_parse(*, df_list, year, config, **_):
     :return: df, parsed and partially formatted to
         flowbyactivity specifications
     """
-    for df in df_list:
-        df= df.query("Country == 'United States of America'")
-        year_list = [range(2012, 2025)]
+    df = pd.concat(df_list)
+    df = df.query("Country == 'United States of America'")
+    # year_list = [str(y) for y in range(2012, 2025)]
 
-        df= df.melt( id_vars=['Country','Category', 'Flow name', 'Flow code', 'Flow unit'], var_name= 'Year', value_name='FlowAmount')
-        df = (df
-              .query("Year ==  ['2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023','2024']")  
-              .rename(columns={'Category':'FlowName','Flow name':'ActivityProducedBy', 'Flow unit':'Unit'})  
-              .assign(ActivityProducedBy = lambda x: x['ActivityProducedBy'].str.strip())
-              .assign(ActivityProducedBy = lambda x: x['FlowName'] + ', '+ x['ActivityProducedBy'])  
-              .drop(columns= ['Country', 'Flow code'])
-              .reset_index(drop= True)
-              )
-        if len(df) == 0:
-            log.warning(' Data is missing')
+    df = df.melt(id_vars=['Country','Category', 'Flow name', 'Flow code', 'Flow unit'],
+                 var_name= 'Year', value_name='FlowAmount')
+    df = (df
+          # .query("Year in @year_list")  
+          .rename(columns={'Category':'FlowName',
+                           'Flow name':'ActivityProducedBy', 
+                           'Flow unit':'Unit'})  
+          .assign(ActivityProducedBy = lambda x: x['ActivityProducedBy'].str.strip())
+          .assign(ActivityProducedBy = lambda x: x['FlowName'] + ', '+ x['ActivityProducedBy'])  
+          .drop(columns= ['Country', 'Flow code'])
+          .reset_index(drop= True)
+          )
+    if len(df) == 0:
+        log.warning('Data is missing')
 
-# hard code data
-    df['Year'] = df['Year'].astype(str)
+    # hard code data
     df['SourceName'] = 'UNEP_IRP_GMFD'
     df['FlowType'] = 'ELEMENTARY_FLOW'
     df['Location'] = '00000'
     df['Unit'] = 'MT'
+    df = assign_fips_location_system(df, 2024)
     
-# Add tmp DQ scores
+    # Add tmp DQ scores
     df['DataReliability'] = 5
     df['DataCollection'] = 5
     df['Compartment'] = 'resource'
@@ -71,5 +73,3 @@ if __name__ == "__main__":
     for y in range(2012, 2025):
         fba = pd.concat([fba, flowsa.getFlowByActivity('UNEP_IRP_GMFD', year=y)],
                         ignore_index=True)
-
-
