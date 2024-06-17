e7d35a149cf327a6eb3ecd43b9c06edc69c8326d
Ben Young <Ben.Young@erg.com>
2024-06-17 10:12:50 -0400

    limit numpy < 2.0.0 #14

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "kaleido==0.1.0.post1;platform_system=='Windows'",
         "kaleido==0.2.0;platform_system=='Linux' or platform_system=='Darwin'",
         'matplotlib>=3.4.3',
-        'numpy>=1.20.1',
+        'numpy>=1.20.1, <2.0.0',
         'openpyxl>=3.0.7',
         'pandas>=1.4.0, <2.1.0',
         'pip>=9',
