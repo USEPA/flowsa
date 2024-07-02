893b68e9603f360ca7987804ad6adb22eea52e69
Ben Young <Ben.Young@erg.com>
2024-07-02 10:35:43 -0400

    consolidate parsing functions by specifying variable names in config

--- flowsa/data_source_scripts/Census_ACS.py
+++ flowsa/data_source_scripts/Census_ACS.py
@@ -2,15 +2,26 @@
 # !/usr/bin/env python3
 # coding=utf-8
 """
-Pulls American Community Survey 5-yr Data Profile (DP03): Selected Economic Characteristics
---year = 'year' e.g. 2015
+U.S. Census American Community Survey
+5-yr Data Profile (DP03): Selected Economic Characteristics
+5-yr Data Profile (DP04): Selected Housing Characteristics
+5-yr Data Profile (DP05): Demographic and Housing Estimates
+5-yr Data Profile (S1501): Educational Attainment
+
+variables: https://api.census.gov/data/2022/acs/acs5/profile/variables.html
 """
 import json
 import pandas as pd
 import numpy as np
+from esupy.remote import make_url_request
 from flowsa.location import US_FIPS
 from flowsa.flowbyfunctions import assign_fips_location_system
 
+
+def load_acs_variables():
+    data = make_url_request('https://api.census.gov/data/2022/acs/acs5/profile/variables.json')
+    variables = json.loads(data.content).get('variables')
+    return variables
 
 def DP_URL_helper(*, build_url, year, **_):
     """
@@ -46,8 +57,15 @@ def DP_call(*, resp, **_):
                       columns=json_data[0])
     return df
 
+def parse_years(year):
+    if '-' in str(year):
+        years = str(year).split('-')
+        year_iter = list(range(int(years[0]), int(years[1]) + 1))
+        year_iter = [str(x) for x in year_iter]
+    return year_iter
 
-def DP03_5yr_parse(*, df_list, year, **_):
+
+def DP_5yr_parse(*, df_list, config, year, **_):
     """
     Combine, parse, and format the provided dataframes
     :param df_list: list of dataframes to concat and format
@@ -55,30 +73,33 @@ def DP03_5yr_parse(*, df_list, year, **_):
     :return: df, parsed and partially formatted to
         flowbyactivity specifications
     """
+    # limit variable dictionary for select years
+    year_dict = {k: v for k,v in config.get('variables').items()
+                 if year in parse_years(v.get('years', [year]))}
+    description = {k: v['label'] for k,v in load_acs_variables().items()
+                   if k in year_dict.keys()}
+    names = {k: v['name'] for k,v in year_dict.items()}
+    units = {k: v['unit'] for k,v in year_dict.items()}
+
     # concat dataframes
     df = pd.concat(df_list, sort=False)
 
     # remove first string of GEO_ID to access the FIPS code
     df['GEO_ID'] = df.GEO_ID.str.replace('0500000US' , '')
-
-    # rename columns to increase readibility
-    df = df.rename(columns={'DP03_0062E':'MHI',
-                            'DP03_0128PE':'Percent Below Poverty Line',
-                            'DP03_0009PE':'Unemployment Rate',
-                            'NAME': 'County Name',
-                            'GEO_ID':'Location'})
 
     # melt economic columns into one FlowAmount column
-    df = df.melt(id_vars= ['Location'], 
-                 value_vars=['MHI', 'Percent Below Poverty Line', 'Unemployment Rate'],
-                 var_name='FlowName',
-                 value_name='FlowAmount')
-    
-    # assign units based on the FlowName values
-    df.loc[df.FlowName == 'MHI', 'Unit'] = 'USD'
-    df.loc[df.FlowName == 'Percent Below Poverty Line', 'Unit'] = '% of people'
-    df.loc[df.FlowName == 'Unemployment Rate', 'Unit'] = '% of unemployed people' #as a percentage of the civilian labor force
-
+    df = (df.melt(id_vars= ['GEO_ID'], 
+                  value_vars=year_dict.keys(),
+                  var_name='code',
+                  value_name='FlowAmount')
+          .assign(FlowName = lambda x: x['code'].map(names))
+          .assign(Unit = lambda x: x['code'].map(units))
+          .assign(Description = lambda x: x['code'].map(description))
+          .assign(Description = lambda x: 
+                  x[['code', 'Description']].agg(': '.join, axis=1)
+                  .str.replace('!!', ', '))
+          .rename(columns={'GEO_ID':'Location'})
+          )
 
     # hard code data for flowsa format
     df['LocationSystem'] = 'FIPS'
@@ -86,8 +107,7 @@ def DP03_5yr_parse(*, df_list, year, **_):
     df['Class'] ='Other'
     df['Year'] = year
     df['ActivityProducedBy'] = 'Households'
-    df['SourceName'] = 'Census_DP03_5yr'
-    df['Description'] = 'Economic data from 5yr DP03'
+    df['SourceName'] = 'Census_ACS'
     # Add tmp DQ scores
     df['DataReliability'] = 5
     df['DataCollection'] = 5
@@ -95,134 +115,14 @@ def DP03_5yr_parse(*, df_list, year, **_):
 
     return df
 
-def DP04_5yr_parse(*, df_list, year, **_):
-    """
-    Combine, parse, and format the provided dataframes
-    :param df_list: list of dataframes to concat and format
-    :param year: year
-    :return: df, parsed and partially formatted to
-        flowbyactivity specifications
-    """
-    # concat dataframes
-    df = pd.concat(df_list, sort=False)
-
-    # remove first string of GEO_ID to access the FIPS code
-    df['GEO_ID'] = df.GEO_ID.str.replace('0500000US' , '')
-
-    if year in ['2010','2011','2012','2013','2014']:
-        df = df.rename(columns={'DP04_0045PE':'FlowAmount', 
-                                'NAME': 'County Name',
-                                'GEO_ID':'Location'})
-    else:
-        df = df.rename(columns={'DP04_0046PE':'FlowAmount',
-                                'NAME': 'County Name',
-                                'GEO_ID':'Location'})
-    
-    # hard code data for flowsa format
-    df['FlowName'] = 'Owner-occupied housing'
-    df['Unit'] = '% of homes' # percent of occupied homes
-    df['LocationSystem'] = 'FIPS'
-    df['FlowType'] = 'TECHNOSPHERE_FLOW'
-    df['Class'] ='Other'
-    df['Year'] = year
-    df['ActivityProducedBy'] = 'Households'
-    df['SourceName'] = 'Census_DP04_5yr'
-    df['Description'] = 'Housing data from 5yr DP04'
-    # Add tmp DQ scores
-    df['DataReliability'] = 5
-    df['DataCollection'] = 5
-    df['Compartment'] = None
-
-    return df
-
-def DP05_5yr_parse(*, df_list, year, **_):
-    """
-    Combine, parse, and format the provided dataframes
-    :param df_list: list of dataframes to concat and format
-    :param year: year
-    :return: df, parsed and partially formatted to
-        flowbyactivity specifications
-    """
-    # concat dataframes
-    df = pd.concat(df_list, sort=False)
-
-    # remove first string of GEO_ID to access the FIPS code
-    df['GEO_ID'] = df.GEO_ID.str.replace('0500000US' , '')
-
-    # rename columns to increase readibility
-    df = df.rename(columns={'DP05_0001E':'FlowAmount', #Total Population, ACS
-                            'NAME': 'County Name',
-                            'GEO_ID':'Location'})
-    
-    
-    # hard code data for flowsa format
-    df['FlowName'] = 'Total Population'
-    df['Unit'] = '# of people'
-    df['LocationSystem'] = 'FIPS'
-    df['FlowType'] = 'TECHNOSPHERE_FLOW'
-    df['Class'] ='Other'
-    df['Year'] = year
-    df['ActivityProducedBy'] = 'Households'
-    df['SourceName'] = 'Census_DP05_5yr'
-    df['Description'] = 'Total Population data from 5yr DP05'
-    # Add tmp DQ scores
-    df['DataReliability'] = 5
-    df['DataCollection'] = 5
-    df['Compartment'] = None
-
-    return df
-
-def S1501_5yr_parse(*, df_list, year, **_):
-    """
-    Combine, parse, and format the provided dataframes
-    :param df_list: list of dataframes to concat and format
-    :param year: year
-    :return: df, parsed and partially formatted to
-        flowbyactivity specifications
-    """
-    # concat dataframes
-    df = pd.concat(df_list, sort=False)
-
-    # remove first string of GEO_ID to access the FIPS code
-    df['GEO_ID'] = df.GEO_ID.str.replace('0500000US' , '')
-
-    if year in ['2010','2011','2012','2013','2014']:
-        df = df.rename(columns={'S1501_C01_014E':'High School Degree',
-                                'S1501_C01_015E':'Bachelors Degree',
-                                'NAME': 'County Name',
-                                'GEO_ID':'Location'})
-    else:
-        df = df.rename(columns={'S1501_C02_014E':'High School Degree',
-                                'S1501_C02_015E':'Bachelors Degree',
-                                'NAME': 'County Name',
-                                'GEO_ID':'Location'})
-    
-    # melt economic columns into one FlowAmount column
-    df = df.melt(id_vars= ['Location'], 
-                 value_vars=['High School Degree', 'Bachelors Degree'],
-                 var_name='FlowName',
-                 value_name='FlowAmount')
-    
-    # assign units based on the FlowName values
-    df.loc[df.FlowName == 'High School Degree', 'Unit'] = '% of people'
-    df.loc[df.FlowName == 'Bachelors Degree', 'Unit'] = '% of people'
-    
-    # hard code data for flowsa format
-    df['LocationSystem'] = 'FIPS'
-    df['FlowType'] = 'TECHNOSPHERE_FLOW'
-    df['Class'] ='Other'
-    df['Year'] = year
-    df['ActivityProducedBy'] = 'Households'
-    df['SourceName'] = 'Census_S1501_5yr'
-    df['Description'] = 'Educational Attainment data from 5yr S1501'
-    # Add tmp DQ scores
-    df['DataReliability'] = 5
-    df['DataCollection'] = 5
-    df['Compartment'] = None
-
-    return df
 
 if __name__ == "__main__":
     import flowsa
     flowsa.generateflowbyactivity.main(source='Census_DP03_5yr', year=2018)
     fba = flowsa.getFlowByActivity('Census_DP03_5yr', year=2018)
+    flowsa.generateflowbyactivity.main(source='Census_DP04_5yr', year=2018)
+    fba2 = flowsa.getFlowByActivity('Census_DP04_5yr', year=2018)
+    flowsa.generateflowbyactivity.main(source='Census_DP05_5yr', year=2018)
+    fba3 = flowsa.getFlowByActivity('Census_DP05_5yr', year=2018)
+    flowsa.generateflowbyactivity.main(source='Census_DP05_5yr', year=2018)
+    fba3 = flowsa.getFlowByActivity('Census_DP05_5yr', year=2018)
