aca1adcab243bed90af24352050321326768bc2c
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-02-25 21:12:41 -0700

    include option for 2022 NAICS year in function description

--- flowsa/flowbyactivity.py
+++ flowsa/flowbyactivity.py
@@ -372,7 +372,7 @@ class FlowByActivity(_FlowBy):
 
     def map_to_sectors(
             self: 'FlowByActivity',
-            target_year: Literal[2002, 2007, 2012, 2017],
+            target_year: Literal[2002, 2007, 2012, 2017, 2022],
             external_config_path: str = None
     ) -> 'FlowByActivity':
         """
