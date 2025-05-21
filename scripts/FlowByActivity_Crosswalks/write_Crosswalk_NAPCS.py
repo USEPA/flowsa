7d69035bc5a1bc7c7e5f55b12f62bd4dcf979727
Ben Young <Ben.Young@erg.com>
2025-05-21 16:33:49 -0400

    revise approach to NAPCS mapping file, including only wholesale (4*) and retail (5*) NAPCS

--- scripts/FlowByActivity_Crosswalks/write_Crosswalk_NAPCS.py
+++ scripts/FlowByActivity_Crosswalks/write_Crosswalk_NAPCS.py
@@ -3,27 +3,52 @@
 # coding=utf-8
 
 """
-Create a crosswalk for NAPCS to NAICS.
+Create a crosswalk for NAPCS to NAICS for wholesaleling and retailing based on
+the highest value wholesale and retail sector identified
 """
 import pandas as pd
+import flowsa
+from flowsa.common import load_crosswalk
 from flowsa.settings import datapath
-from scripts.FlowByActivity_Crosswalks.common_scripts import order_crosswalk
 
 
 if __name__ == '__main__':
 
-    cw = (pd.read_csv(datapath / 'NAICS_to_NAPCS_Crosswalk_2017.csv')
-          .filter(['NAPCS_2017_Code', 'NAPCS_2017_Name', 'NAICS_2017_Code'])
-          .rename(columns={'NAPCS_2017_Code':'Activity',
-                           'NAICS_2017_Code': 'Sector',
-                           'NAPCS_2017_Name':'Note'})
-          .drop_duplicates()
+    fba = flowsa.getFlowByActivity('Census_EC_PxI', 2017)
+    napcs = ('4', '5')
+    sectors = ('42', '44', '45')
+    result = (fba
+              .query('ActivityProducedBy != "00" and '
+                     'FlowName.str.startswith(@napcs)')
+              .query('ActivityProducedBy.str.startswith(@sectors)')
+              .reset_index(drop=True)
+              )
+
+    # Find the sector with the greatest prevalence of that NAPCS code to assign mapping
+    result = (result.loc[result.groupby('FlowName')['FlowAmount'].idxmax()]
+              .filter(['FlowName', 'Description', 'ActivityProducedBy'])
+              .sort_values(by='FlowName')
+              .reset_index(drop=True)
+              .merge(load_crosswalk('Sector_2017_Names'),
+                     left_on='ActivityProducedBy',
+                     right_on='NAICS_2017_Code', how='left')
+              )
+
+    cw = (result
+          .drop(columns=['NAICS_2017_Code'])
+          .rename(columns={'FlowName':'Activity',
+                           'ActivityProducedBy': 'Sector',
+                           'Description':'NAPCS_Description'})
           .assign(ActivitySourceName = 'NAPCS_2017')
           .assign(SectorSourceName = 'NAICS_2017_Code')
           .assign(SectorType = '')
           )
+    col_order = ['ActivitySourceName'] + \
+                ['Activity', 'SectorSourceName', 'Sector', 'SectorType'] + \
+                ['NAPCS_Description', 'NAICS_2017_Name']
 
-    df = order_crosswalk(cw)
+    df = cw[col_order]
+
     # save as csv
     df.to_csv(f"{datapath}/activitytosectormapping/NAICS_Crosswalk_NAPCS_2017.csv",
               index=False)
