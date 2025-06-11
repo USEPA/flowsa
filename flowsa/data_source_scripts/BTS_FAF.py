20f40fc6b46a0f1a022a2bb829873c07bc52422a
Ben Young <Ben.Young@erg.com>
2025-06-10 22:28:36 -0400

    draft FAF fba

---
+++ flowsa/data_source_scripts/BTS_FAF.py
@@ -0,0 +1,130 @@
+# BTS_FAF.py (flowsa)
+# !/usr/bin/env python3
+# coding=utf-8
+"""
+Pulls ORNL Freight Analysis Framework dataset
+"""
+
+import pandas as pd
+import zipfile
+import tabula
+
+import io
+import requests
+from esupy.remote import make_url_request
+from flowsa.settings import externaldatapath
+from flowsa.flowbyfunctions import assign_fips_location_system
+from flowsa.location import US_FIPS
+
+
+def faf_call(*, resp, config, **_):
+    """
+    Convert response for calling url to pandas dataframe,
+    begin parsing df into FBA format
+    :param resp: df, response from url call
+    :return: pandas dataframe of original source data
+    """
+    zipf = "FAF5.6.1_State_2018-2023.zip"
+    file = 'FAF5.6.1_State_2018-2023.csv'
+
+    ## do we need the state database?
+    # url = 'https://www.bts.gov/sites/bts.dot.gov/files/2024-03/FAF4.5.1_csv_2013-2018.zip'
+    # year = 2016
+    # file = f'FAF4.5.1_{year}.csv'
+
+    # resp = make_url_request(url)
+    # with zipfile.ZipFile(io.BytesIO(resp.content), "r") as zf:
+    #     with zf.open(file) as csvf:
+    #         df = pd.read_csv(csvf)
+
+    with zipfile.ZipFile(externaldatapath / zipf, "r") as zf:
+        with zf.open(file) as csvf:
+            df = pd.read_csv(csvf)
+
+    return [df]
+
+
+def faf_parse(*, df_list, **_):
+    """
+    Combine, parse, and format the provided dataframes
+    :param df_list: list of dataframes to concat and format
+    :param args: dictionary, used to run generateflowbyactivity.py
+        ('year' and 'source')
+    :return: df, parsed and partially formatted to
+        flowbyactivity specifications
+    """
+
+    df_list = faf_call(resp=None, config=None)
+
+    # URL of the PDF
+    pdf = "FAF5 User Guide.pdf"
+    # pdf = 'https://faf.ornl.gov/faf5/data/FAF5%20User%20Guide.pdf'
+    # Specify the page number
+    page_number = 8  # Change this to the desired page number
+    # Read the table from the specified page
+    tables = tabula.read_pdf(externaldatapath / pdf, pages=page_number)
+    # Convert the first table to a DataFrame
+    codes = tables[0] if tables else pd.DataFrame()
+
+    tables2 = tabula.read_pdf(externaldatapath / pdf, pages=7)
+    modes = tables2[0] if tables2 else pd.DataFrame()
+    modes = modes.dropna(subset='Code')
+
+    df0 = pd.concat(df_list)
+    # https://faf.ornl.gov/faf5/data/FAF5%20User%20Guide.pdf
+    df_list=[]
+    for year in range(2018, 2024):
+        df1 = (df0
+               .filter([
+                    # 'fr_orig', 'dms_origst', 'dms_destst', 'fr_dest',
+                    # 'fr_inmode', 'fr_outmode', 'dist_band',
+                    'dms_mode', 'sctg2', 'trade_type',
+                    f'tons_{year}', f'current_value_{year}', f'tmiles_{year}'])
+               .query('trade_type == 1') # Domestic only
+               )
+
+        df1 = (df1
+               # .drop(columns=['fr_orig', 'dms_origst', 'dms_destst']) # drop foreign source and state-to-state details
+               .pivot_table(index=['sctg2', 'dms_mode'], # pivot on good (sctg2) and mode
+                            aggfunc='sum')
+               .reset_index()
+               .merge(codes.rename(columns={'Code': 'sctg2'}), on='sctg2', how='left')
+               .merge(modes.rename(columns={'Code': 'dms_mode'}), on='dms_mode', how='left')
+               .drop(columns=['Description'])
+               )
+        df1 = (df1
+               .melt(id_vars=['sctg2', 'dms_mode', 'Mode', 'Commodity Description'],
+                     value_vars=[f'tons_{year}', f'current_value_{year}', f'tmiles_{year}'],
+                     value_name='FlowAmount')
+               .assign(Year = str(year))
+               .assign(Unit = lambda x: x['variable'].replace(
+                   {f'tons_{year}': 'tons',
+                    f'current_value_{year}': 'current value',
+                    f'tmiles_{year}': "ton-miles"}))
+               .drop(columns=['variable'])
+               .rename(columns={'Commodity Description': 'FlowName',
+                                'Mode': 'ActivityProducedBy'})
+               )
+        df_list.append(df1)
+    df = pd.concat(df_list)
+
+    df = (df
+          .assign(Class = "Other")
+          .assign(SourceName = 'BTS_FAF')
+          .assign(Compartment = None)
+          .assign(FlowType = "TECHNOSPHERE_FLOW")
+          # .assign(Description = '')
+          .assign(Location = US_FIPS)
+          .pipe(assign_fips_location_system, 2024)
+    # Temp data quality
+          .assign(DataReliability = 5)
+          .assign(DataCollection = 5)
+          .drop(columns=['sctg2', 'dms_mode'])
+          )
+
+    return df
+
+if __name__ == "__main__":
+    import flowsa
+    flowsa.generateflowbyactivity.main(source='BTS_FAF', year='2018-2023')
+    fba = flowsa.getFlowByActivity('BTS_FAF', 2022)
