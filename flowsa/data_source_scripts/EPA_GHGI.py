3ac8e1eff27322a05df334b5e1b459eb72e203b0
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-10-28 20:39:30 -0600

    address numpy.exceptions.DTypePromotionError: The DType <class 'numpy.dtypes.StrDType'> could not be promoted by <class 'numpy.dtypes._PyFloatDType'>

--- flowsa/data_source_scripts/EPA_GHGI.py
+++ flowsa/data_source_scripts/EPA_GHGI.py
@@ -496,20 +496,15 @@ def ghg_parse(*, df_list, year, config, **_):
         # set suppressed values to 0 but mark as suppressed
         # otherwise set non-numeric to nan
         try:
-            df = (df.assign(
-                    Suppressed = np.where(df.FlowAmount.str.strip() == "+", "+",
-                                          np.nan),
-                    FlowAmount = pd.Series(
-                        np.where(df.FlowAmount.str.strip() == "+", 0,
-                                 df.FlowAmount.str.replace(',',''))))
-                )
-            df = (df.assign(
-                    FlowAmount = np.where(pd.to_numeric(
-                        df.FlowAmount, errors='coerce').isnull(),
-                                          np.nan, pd.to_numeric(
-                                              df.FlowAmount, errors='coerce')))
-                .dropna(subset='FlowAmount')
-                )
+            df['Suppressed'] = (df['FlowAmount']
+                                .astype(str).str.strip().eq('+')
+                                .replace({True: '+', False: np.nan})
+                                .infer_objects(copy=False)
+                                )
+            df['FlowAmount'] = df['FlowAmount'].astype(str).str.replace(',', '').infer_objects(copy=False)
+            df['FlowAmount'] = df['FlowAmount'].replace('+', '0').infer_objects(copy=False)
+            df['FlowAmount'] = pd.to_numeric(df['FlowAmount'], errors='coerce')
+            df = df.dropna(subset='FlowAmount')
         except AttributeError:
             # if no string in FlowAmount, then proceed
             df = df.dropna(subset='FlowAmount')
